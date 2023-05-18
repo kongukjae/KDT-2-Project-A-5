e0e605385d3949195ae766d0cd2fe6971790c4ea
jangrubin <wkdfnqls2465@gmail.com>
2023-05-18 15:18:24 +0900

    서버 호출

--- server.js
+++ server.js
@@ -3,3 +3,9 @@ import http from "http";
 const server = http.createServer((req, res)=> {
   
 });
+server.listen(8080,(err)=> {
+  if(err) {
+    console.log(err);
+  }
+  console.log("서버가 정상 연결 됐습니다");
+})
