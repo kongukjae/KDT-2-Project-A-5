62336d96dcdd1c4681338d5a350d65566f9d90fc
jangrubin <wkdfnqls2465@gmail.com>
2023-05-18 15:18:47 +0900

    서버 GET, POST 분기 처리

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 import http from "http";
 
 const server = http.createServer((req, res)=> {
-  
+  if(req.method === 'GET') {
+    
+  }
+  if(req.method === 'POST') {
+    
+  }
 });
 server.listen(8080,(err)=> {
   if(err) {
