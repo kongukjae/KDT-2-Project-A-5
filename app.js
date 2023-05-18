7e81dda50d756f43d01e3152c6db089628d824c6
jangrubin <wkdfnqls2465@gmail.com>
2023-05-18 14:55:49 +0900

    서버 호출 부분 추가

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import http from "http";
-
+// 서버
 const server = http.createServer((req, res) => {
   if(req.method === 'GET') {
 
@@ -7,4 +7,11 @@ const server = http.createServer((req, res) => {
   if(req.method === 'POST') {
 
   }
+});
+server.listen(8080, (err)=> {
+  if(err) {
+    console.log(err);
+  }
+  console.log("서버가 정상적으로 연결 됐습니다.");
 })
+// 서버 호출
