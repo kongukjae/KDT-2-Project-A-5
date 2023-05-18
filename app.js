5ecea6f6225c13a7412ca7c9b347d13ced29266d
jangrubin <wkdfnqls2465@gmail.com>
2023-05-18 14:57:48 +0900

    최초 접속시 보일 페이지 테스트

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ import http from "http";
 // 서버
 const server = http.createServer((req, res) => {
   if(req.method === 'GET') {
-
+    if(req.url === '/') {
+      res.writeHead(200, {'Content-Type' : 'text/html'});
+      res.write("HelloWorld");
+      res.end();
+    }
   }
   if(req.method === 'POST') {
 
