c52bb57a48f018889fef33ca6f93645bedec696f
jangrubin <wkdfnqls2465@gmail.com>
2023-05-19 15:27:10 +0900

    Get 데이터 받을 준비

--- src/controllers/app.js
+++ src/controllers/app.js
@@ -47,7 +47,11 @@ const server = http.createServer((req, res)=> {
       )
       res.end();
     }
-
+    if(req.url ==='test') {
+      // 클라이언트로 부터 받은 데이터를 ?를 기준으로 나누려고 함
+      const getData = req.url.split('?');
+      // 로직 들어갈 자리고
+    }
   }
   if(req.method === 'POST') {
   }} catch (err) {
