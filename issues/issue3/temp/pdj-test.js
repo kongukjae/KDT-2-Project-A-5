f73fae7ed6e0b21eafcb2bbc184cb5d30cdfa388
박준형 <108733795+krapli441@users.noreply.github.com>
2023-05-19 14:52:28 +0900

    utils 폴더 안에 tagMaker.js 파일 추가. issues/issue3/temp 폴더 안에 개인 작업용 pdj-test.js 파일 생성.

---
+++ issues/issue3/temp/pdj-test.js
@@ -0,0 +1,68 @@
+import all_mighty_editor from "../../../utils/all-mighty-editor.js";
+import tagMaker from "../../../utils/tagMaker.js";
+const {
+  multiAndSingleTagMaker,
+  positionEditor,
+  fontAndLayoutEditor,
+  kingGodFlexEditor,
+  allMightyStyleEditor,
+} = all_mighty_editor;
+
+const root = document.getElementById("root");
+kingGodFlexEditor(root, "row", "center", "center");
+
+//* 엘리먼트 생성 페이지
+const mainDIv = multiAndSingleTagMaker(root, "div", "main-div", 1, (ele) => {});
+const mainTextContainer = multiAndSingleTagMaker(
+  mainDIv,
+  "div",
+  "main-container"
+);
+kingGodFlexEditor(mainTextContainer, "column", "", "");
+const mainTextTitle = multiAndSingleTagMaker(
+  mainTextContainer,
+  "div",
+  "main-text-title",
+  1,
+  (ele) => {
+    ele.textContent = "하이";
+  }
+);
+const mainTextSub = multiAndSingleTagMaker(
+  mainTextContainer,
+  "div",
+  "main-text-sub",
+  1,
+  (ele) => {
+    ele.textContent = "하이";
+  }
+);
+const mainTextImg = multiAndSingleTagMaker(
+  mainTextContainer,
+  "img",
+  "main-text-img",
+  1,
+  (ele) => {
+    ele.src = "/src/views/img/img-13.png";
+  }
+);
+
+//* CSS 부분
+const mainDIvCss = {
+  width: "430px",
+  height: "932px",
+  border: "1px solid black",
+  margin: "10px",
+};
+const mainTextContainerCss = {};
+const mainTextTitleCss = { fontSize: "24px", backgroundColor: "red" };
+const mainTextSubCss = { fontSize: "64px", backgroundColor: "skyblue" };
+const mainTextImgCss = { /*  width: "430px", height: "60%", */ scale: "0.2" };
+
+//* 그 외 엘리먼트 스타일 수정
+allMightyStyleEditor(mainDIv, mainDIvCss, (ele) => {
+  console.dir(ele);
+});
+allMightyStyleEditor(mainTextTitle, mainTextTitleCss, (ele) => {});
+allMightyStyleEditor(mainTextSub, mainTextSubCss, (ele) => {});
+allMightyStyleEditor(mainTextImg, mainTextImgCss, (ele) => {});
