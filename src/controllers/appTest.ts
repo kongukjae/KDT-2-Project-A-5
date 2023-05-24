f3eed46b52cc9548828006a6475c42b0061fa7f0
gwonyejun <kyjuni1059@gmail.com>
2023-05-24 16:02:49 +0900

    Ts, 리액트 Fixes #11 ts파일로 변환 해보기

--- src/controllers/appTest.ts
+++ src/controllers/appTest.ts
@@ -16,8 +16,8 @@ let pokeJSONFile = fs.readFileSync(
 let parsedPoke = JSON.parse(pokeJSONFile);
 
 // client input 포켓몬 데이터 처리 함수
-function findPokeMoudle(urlValue : string) {
-  const result = parsedPoke.filter((x : string) => {
+function findPokeMoudle(urlValue: string) {
+  const result = parsedPoke.filter((x: string) => {
     if (x === urlValue) {
       return true;
     }
@@ -64,7 +64,7 @@ const server = http.createServer((req: http.IncomingMessage, res: http.ServerRes
         res.end();
       }
       // js파일 요청
-      if (req.url.endsWith(".js")) {
+      if (req.url && req.url.endsWith(".js")) {
         res.writeHead(200, { "Content-Type": "text/javascript" });
         res.write(fs.readFileSync(path.join(root, req.url)));
         // res.write(
@@ -73,29 +73,29 @@ const server = http.createServer((req: http.IncomingMessage, res: http.ServerRes
         res.end();
       }
       // png 파일 요청
-      if (req.url.endsWith(".png")) {
+      if (req.url && req.url.endsWith(".png")) {
         res.writeHead(200, { "Content-Type": "image/png" });
         res.write(fs.readFileSync(path.join(root, req.url)));
         res.end();
       }
-      if (req.url.includes("/name")) {
+      if (req.url && req.url.includes("/name")) {
         // console.log(req.url)
-
-        const parseData = qs.parse(req.url, true);
+        const parseData = qs.parse(req.url as string,{ignoreQueryPrefix : true});
         // console.log(parseData);
         const valueData = Object.values(parseData);
         // console.log(valueData[0]);
-        const printData = findPokeMoudle(valueData[0]);
+        const printData = findPokeMoudle(valueData[0] as string); 
         console.log("모듈 결과 값" + printData);
         // res.writeHead(200, {'Content-Type' : 'text/html'})
         // res.write("hello");
         res.end(`${printData}`);
-        // console.log("이거는 url이 파싱된 데이터 :" + parseData);
+        //  console.log("이거는 url이 파싱된 데이터 :" + parseData);
         // const parseData = qs.parse(req.url, true);
         // const urlValue = Object.values(parseData);
         // console.log(urlValue);
         // console.log(findPokeMoudle(urlValue[0]));
         // res.write(`test${findPokeMoudle(urlValue[0])}`);
+
       }
     }
     if (req.method === "POST") {
@@ -107,9 +107,7 @@ const server = http.createServer((req: http.IncomingMessage, res: http.ServerRes
     res.end();
   }
 });
-server.listen(8080, (err) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log("서버가 정상 연결 됐습니다");
+server.listen(8080, () => {
+  console.log("서버가 정상 연결됐습니다");
 });
+// Error | null
