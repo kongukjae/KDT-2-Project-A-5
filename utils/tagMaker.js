30957ab71f6aaeb39c7414efb141929e2c38a817
ParkDalJae <dv.over931@gmail.com>
2023-05-25 17:21:25 +0900

    tagMaker.js TS로 전환 전환 후 js 컴파일 app.js 동작 확인

--- utils/tagMaker.js
+++ utils/tagMaker.js
@@ -1,8 +1,11 @@
-export default function (tagName, parent, attribute) {
-  const element = document.createElement(tagName);
+"use strict";
+function default_1(tagName, parent, attribute) {
+  var element = document.createElement(tagName);
   parent.appendChild(element);
   if (attribute && typeof attribute === "object") {
     Object.assign(element, attribute);
   }
   return element;
 }
+const _default = default_1;
+export { _default as default };
