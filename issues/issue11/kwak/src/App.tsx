bbba12377414170ac44913969b6583509f88bde3
KwakYounHo <kwakyh951@gmail.com>
2023-05-30 15:53:21 +0900

    react 문법 사용해 보기

--- issues/issue11/kwak/src/App.tsx
+++ issues/issue11/kwak/src/App.tsx
@@ -1,5 +1,31 @@
 import React from 'react';
 
-const App = ()=><>Hello world</>;
+function FirstTest() {
+  return (
+    <ul>
+      <li>첫번째</li>
+      <li>두번째</li>
+      <li>세번째</li>
+      <li>네번째</li>
+      <li>다섯번째</li>
+    </ul>
+  )
+};
+
+function SeconfTest() {
+  return (
+    <a href="loginGo?">위로 가기</a>
+  )
+}
+
+const App = ()=> {
+  return (
+    <div>
+      <>Hello world</>
+      <FirstTest />
+      <SeconfTest />
+    </div>
+  )
+};
 
 export default App;
