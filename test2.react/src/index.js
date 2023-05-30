eece56d8714fcbe5d3e804f08f8c0534a5785d05
wjkk21 <verstand821@gmail.com>
2023-05-30 15:52:47 +0900

    보류

--- test2.react/src/index.js
+++ test2.react/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import css from './app.css'
 // 리액트 컴포넌트 작성
 function App() {
   return (
     <div>
-      <h1>Hello, React!</h1>
+      <h1 style={css}>Hello, React!</h1>
     </div>
   );
 }
