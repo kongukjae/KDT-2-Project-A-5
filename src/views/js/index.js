60826386a81f8fc37acf3b4abe2735e925d5c487
KwakYounHo <kwakyh951@gmail.com>
2023-05-31 12:59:58 +0900

    주석 제거

--- src/views/js/index.js
+++ src/views/js/index.js
@@ -3,16 +3,5 @@ import React from "react";
 import { createRoot } from "react-dom/client"
 import App from "./App"
 
-// ReactDOM.render(
-  // <article>
-    {/* <Header /> */}
-    {/* <div className="main"> */}
-      {/* <Main style={{ color: "red" }} /> */}
-    {/* </div> */}
-  {/* </article>, */}
-  // document.getElementById("root")
-// );
-
 const root = createRoot(document.getElementById("root"));
-// const root = document.getElementById("root");
 root.render(<App />);
