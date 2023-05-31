4efe2f17f1461910ef0137c32fe454248365a6a9
gwonyejun <kyjuni1059@gmail.com>
2023-05-31 15:20:52 +0900

    6개의 컴포넌트 생성하기 Fixes #16 생성

--- src/views/js/kwon.js
+++ src/views/js/kwon.js
@@ -1,1 +1,10 @@
 import React from "react";
+
+
+export default () => {
+  return React.createElement("h1",   {className:"yejun", style:{
+    backgroundColor:"green",
+    color:"white",
+    innerText:"YeJun",
+  }}, "Hello, React!")
+}
