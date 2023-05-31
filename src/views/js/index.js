4ef4067315b04ee7618e1878847ee84ff03782d6
박준형 <108733795+krapli441@users.noreply.github.com>
2023-05-31 11:49:25 +0900

    컴포넌트?를 몇 개 생성.

--- src/views/js/index.js
+++ src/views/js/index.js
@@ -1,2 +1,15 @@
 import React from "react";
+import { render } from "react-dom";
 
+import header from "./header.js";
+import main from "./main.js";
+
+render(
+  <article>
+    <Header />
+    <div className="main">
+      <Main />
+    </div>
+  </article>,
+  document.getElementById("root")
+);
