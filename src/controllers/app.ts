5009d7b85d5bec28835233af581199a38b0e180b
jangrubin <wkdfnqls2465@gmail.com>
2023-05-31 17:26:19 +0900

    listen의 err부분에서 계속 에러 발생

--- src/controllers/app.ts
+++ src/controllers/app.ts
@@ -1,2 +1,14 @@
 import express, { Request, Response } from 'express';
+
 const app = express();
+// const __filename = fileURLToPath(__dirname(import.meta.url))
+app.use(express.static('/'));
+app.get('/', (req : Request, res : Response)=> {
+  res.send("HelloWorld");
+})
+app.listen(8080, (err : Error) : void => {
+  if(err) {
+    console.log(err);
+  }
+  console.log("서버 접속")
+})
