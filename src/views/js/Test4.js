e2489a65dbb5155caab09add3d0d4fe28c895fc2
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-05-31 15:28:19 +0900

    6개 컴포넌트 생성 후 푸쉬

---
+++ src/views/js/Test4.js
@@ -0,0 +1,7 @@
+import React from 'react';
+function Test4() {
+    return (<div>
+        <p>안녕 대머리야4</p>
+    </div>)
+} 
+export default Test4;
---
+++ src/views/js/Test5.js
@@ -0,0 +1,7 @@
+import React from 'react';
+function Test5() {
+    return (<div>
+        <p>안녕 대머리야5</p>
+    </div>)
+} 
+export default Test5;
