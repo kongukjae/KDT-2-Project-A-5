ddedb8ed05f7527a9a4fc158df360d11da46c042
박준형 <108733795+krapli441@users.noreply.github.com>
2023-06-01 11:30:23 +0900

    샘플 텍스트 추가. 확인 안됨. 점검 필요.

--- src/views/js/header.tsx
+++ src/views/js/header.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-
-export default function Header () {
+export default function Header() {
   return (
     <>
-    <div className="header"></div>
+      <div className="header">
+        <div>샘플텍스트</div>
+      </div>
     </>
-  )
+  );
 }
