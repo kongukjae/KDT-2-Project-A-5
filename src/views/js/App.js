2a525482ace6cc59e5ccd8c0033d3c3475066a0f
wjkk21 <verstand821@gmail.com>
2023-06-01 16:30:09 +0900

    되나요?

---
+++ src/views/js/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './header'
+import Main from './main.js';
+import Main2 from './main2';
+
+export default ()=> {
+  return (
+  <div >
+    <Header />
+    <div className="main">
+      <Main style={{ color: "red" }} />
+      <Main2></Main2>
+    </div>
+  </div>
+  )
+}
