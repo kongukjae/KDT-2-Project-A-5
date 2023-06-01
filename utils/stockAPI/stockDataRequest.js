f5fb782e70b572e2d9ef77242bfa2007abe919c5
ParkDalJae <dv.over931@gmail.com>
2023-06-01 16:52:31 +0900

    파일명 수정

--- utils/stockAPI/stockDataRequest.js
+++ utils/stockAPI/stockDataRequest.js
@@ -2,10 +2,9 @@ const axios = require("axios");
 const dotenv = require("dotenv");
 const fs = require("fs");
 const path = require("path");
+dotenv.config({ path: "../../.env" }); // env 경로 설정
 
-dotenv.config({ path: "../../.env" });
 const root = path.join(__dirname, "..", "..");
-console.log(root);
 const apiKey = process.env.apiKey; // 여기에 인증키를 입력하세요
 const maxNum = 2178976; // 토탈 카운트
 const numOfRows = maxNum; // 임의의 숫자로 입력.
