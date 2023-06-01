26500b51cf27ec96341278b9b8294475b01e480b
jangrubin <wkdfnqls2465@gmail.com>
2023-06-01 10:58:38 +0900

    MySQL 연결, 연결 종료 명령문 작성

--- utils/DB/dbConfigure.ts
+++ utils/DB/dbConfigure.ts
@@ -1,8 +1,25 @@
 import mysql from "mysql2"
 
-mysql.createConnection( {
+const dbConnect = mysql.createConnection( {
   host : "",
   user : "",
   password : "",
   database : ""
 });
+// ! DB 연결 명령문
+// dbConnect.connect((err)=> {
+//   if(err) {
+//     console.error("DB연결에 실패했습니다", err);
+//     return;
+//   }
+//   console.log("DB연결에 성공했습니다")
+// })
+// ! DB 연결 끊기 명령문
+// dbConnect.end((err) => {
+//   if (err) {
+//     console.error("DB연결 끊기에 실패했습니다.", err);
+//     return;
+//   }
+//   console.log("DB와의 연결을 성공적으로 끊었습니다");
+// });
+export default dbConnect;
