eb26fb48c158a1e6b24c7e6cef94624b5c153894
박준형 <108733795+krapli441@users.noreply.github.com>
2023-06-01 12:33:19 +0900

    nav.tsx : react-icons를 불러와 svg 아이콘을 추가하였음. style.css : 내비게이션 바 영역에 display:flex, justify-content:space-around, align-items:center 옵션 추가.

--- src/views/js/nav.tsx
+++ src/views/js/nav.tsx
@@ -1,7 +1,15 @@
 import React from "react";
-
+import { BsHouse } from "react-icons/Bs";
+import { BiCar } from "react-icons/Bi";
+import { SlWallet } from "react-icons/Sl/";
 export default function Nav() {
-  return <>
-  <div className="navigation"></div>
-  </>;
+  return (
+    <>
+      <div className="navigation">
+        <BsHouse size={40} color="white" />
+        <BiCar size={40} color="white" />
+        <SlWallet size={40} color="white" />
+      </div>
+    </>
+  );
 }
