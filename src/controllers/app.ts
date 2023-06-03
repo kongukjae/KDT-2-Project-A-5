304981b909a6d5c0122fee25209c7abb98338f06
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-06-03 14:38:54 +0900

    서버 실행후, 3분에 한번씩 주식데이터 요청하기

--- src/controllers/app.ts
+++ src/controllers/app.ts
@@ -20,24 +20,40 @@ dbConnect.connect((err) => {
   console.log("DB연결에 성공했습니다");
 });
 // 최초 접속시에 주식 데이터 요청
-const apiKey = process.env.alphaApiKey; // 여기에 인증키를 입력하세요
-const symbol = "IBM";
-const endpoint = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${symbol}&apikey=${apiKey}`; //벡틱처리 전 풀 주소
-axios
-  .get(endpoint)
-  .then((response: any) => {
-    // 응답을 처리하는 코드
-    const jsonData = response.data;
-    fs.writeFileSync(
-      root + "/stockData.json",
-      JSON.stringify(jsonData, null, 2)
-    ); //저장되는 파일은 .gitignore처리 함
-    console.log(jsonData);
-  })
-  .catch((error: Error) => {
-    // 에러를 처리하는 코드
-    console.error(error);
-  });
+// const apiKey = process.env.alphaApiKey; // 여기에 인증키를 입력하세요
+// const symbol = "IBM";
+// const endpoint = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${symbol}&apikey=${apiKey}`; //벡틱처리 전 풀 주소
+// axios
+//   .get(endpoint)
+//   .then((response: any) => {
+//     // 응답을 처리하는 코드
+//     const jsonData = response.data;
+//     fs.writeFileSync(
+//       root + "/stockData.json",
+//       JSON.stringify(jsonData, null, 2)
+//     ); //저장되는 파일은 .gitignore처리 함
+//     console.log(jsonData);
+//   })
+//   .catch((error: Error) => {
+//     // 에러를 처리하는 코드
+//     console.error(error);
+//   });
+// 알파벤티지에 주식 데이터 요청하는 함순
+  async function stockDataRequest() {
+    try {
+      const symbol = "IBM";
+      const apiKey = process.env.alphaApiKey;
+      const response = await axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${symbol}&apikey=${apiKey}`)
+      const stockData = response.data;
+      console.log(stockData);
+    } catch (error) {
+      console.error('주식 데이터를 받아오는데 실패했습니다', error);
+    }
+    // 3분에 한번씩 주식데이터 요청
+    setTimeout(stockDataRequest, 3 * 60 * 1000);
+  }
+  // 최초 주식 데이터 요청
+  stockDataRequest();
 app.use(express.static(root)); //root 디렉토리
 app.use(express.static(rootPublic)); //root의 하위 디렉토리는 첫번째만 접근 가능하기 때문에 별도로 지정.
 
