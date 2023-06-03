52fb50062ec33f47ce4699b37b9f25920ccbb2c2
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-06-03 14:29:50 +0900

    서버 실행했을 때 알파벤티지에서 주식 데이터 받아오기

--- src/controllers/app.ts
+++ src/controllers/app.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import path from "path";
 import dbConnect from "../../utils/DB/dbConfigure";
+import axios from "axios";
+import dotenv from "dotenv"
+import fs from "fs";
+dotenv.config({ path: "../../.env" }); // env 경로 설정
 const root = path.join(__dirname, "..", ".."); //C:\Users\over9\KDT-2_FullStack\KDT-2-Project-A-5
-console.log(root);
+// console.log(root);
 const rootPublic = path.join(root, "public"); //C:\Users\over9\KDT-2_FullStack\KDT-2-Project-A-5\public
-console.log(rootPublic);
+// console.log(rootPublic);
 
 const app = express();
 // DB 연결
@@ -15,13 +19,34 @@ dbConnect.connect((err) => {
   }
   console.log("DB연결에 성공했습니다");
 });
-
+// 최초 접속시에 주식 데이터 요청
+const apiKey = process.env.alphaApiKey; // 여기에 인증키를 입력하세요
+const symbol = "IBM";
+const endpoint = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${symbol}&apikey=${apiKey}`; //벡틱처리 전 풀 주소
+axios
+  .get(endpoint)
+  .then((response: any) => {
+    // 응답을 처리하는 코드
+    const jsonData = response.data;
+    fs.writeFileSync(
+      root + "/stockData.json",
+      JSON.stringify(jsonData, null, 2)
+    ); //저장되는 파일은 .gitignore처리 함
+    console.log(jsonData);
+  })
+  .catch((error: Error) => {
+    // 에러를 처리하는 코드
+    console.error(error);
+  });
 app.use(express.static(root)); //root 디렉토리
 app.use(express.static(rootPublic)); //root의 하위 디렉토리는 첫번째만 접근 가능하기 때문에 별도로 지정.
 
 app.get("/", (req, res) => {
   console.log({ root: rootPublic });
   res.sendFile(`index.html`, { root: rootPublic });
+
+
+
 });
 
 app.use((req, res) => {
