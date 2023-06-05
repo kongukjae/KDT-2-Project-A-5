3f9f2cad645b0c0d0bb014e2de17c4a9b1acac75
jangrubin <wkdfnqls2465@gmail.com>
2023-06-05 14:27:57 +0900

    contentsboxarea에 소켓, 주식 관련 부분 제거

--- utils/Components/contentsBoxArea.tsx
+++ utils/Components/contentsBoxArea.tsx
@@ -1,16 +1,6 @@
 import React, { useEffect, useState } from "react";
-import io from "socket.io-client";
 import Magic from "./contentsBoxLayout";
-const socket = io("http://localhost:3030");
 
-socket.on("connect", () => {
-  console.log("소켓 서버에 연결되었습니다.");
-});
-
-socket.on("disconnect", () => {
-  console.log("소켓 서버와의 연결이 해제되었습니다.");
-});
-const [stockData, setStockData] = useState(null);
 // 함수의 컴포넌트 타입을 명시하기 위해서이다.
 // 프로퍼티의 키 값이 무엇이 들어 오든 다 받을수 있다.
 interface MyComponentProps {
@@ -49,12 +39,6 @@ const MyComponent: React.FC<MyComponentProps> = (text) => {
     </div>
   );
 };
-useEffect(()=> {
-  socket.on("stockDataUpdate", stockData => {
-    // const parseStockData = JSON.parse(stockData)
-    setStockData(stockData);
-    console.log(setStockData);
-  })
-})
+
 
 export default MyComponent;
