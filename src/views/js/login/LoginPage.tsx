8dc00c976a787906c4682d48610e82956d4a9203
wjkk21 <verstand821@gmail.com>
2023-06-05 12:12:41 +0900

    views/js/login/LoginPage.tsx 생성 Components login.tsx 생성

---
+++ src/views/js/login/LoginPage.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import Login from '../../../../utils/Components/login'
+
+
+const Screen: React.FC = () => {
+  return (
+    <>
+      <Login/>
+    </>
+  );
+};
+
+export default Screen;
