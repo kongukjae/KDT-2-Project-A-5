c08972b961fbb71c7572462b8000b3cefe0ca39b
gwonyejun <kyjuni1059@gmail.com>
2023-06-05 09:36:29 +0900

    주식 데이터 받아와서 클라이언트 화면에 띄워주기 Fixes #18

--- src/controllers/app.ts
+++ src/controllers/app.ts
@@ -20,47 +20,49 @@ dbConnect.connect((err) => {
 
 app.use(express.static(root)); //root 디렉토리
 app.use(express.static(rootPublic)); //root의 하위 디렉토리는 첫번째만 접근 가능하기 때문에 별도로 지정.
-const apiKey = `${process.env.apiKey}`;
-
-const headers = new Headers();
-headers.append('x-api-key', apiKey);
-
-const controller = new AbortController();
-const signal = controller.signal;
-
-const timeout = setTimeout(() => {
-  controller.abort();
-}, 5000);
-
-// ! 애플 데이터를 가지고 와서 
-// fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=AAPL&apikey=${apiKey}`, {
-//   method: 'GET',
-//   headers: headers
-// })
-// .then(response => {
-//   return response.json();
-// })
-// .then(data => {
-//   const datatest = JSON.stringify(data,null,2);
-// fs.writeFile('test.json', datatest, (error) => {
-
-// });
-//   console.log("이게 데이터야",data); // 여기서 데이터를 사용합니다
-// })
-// .catch(error => {
-//   console.error(error);
-// })
-// .finally(() => {
-//   clearTimeout(timeout);
-// });
 
 // 회사 명으로 테이블을 생성을 하고 데이터를 날짜 별로 튜플을 생성을 하였다.
 addData();
 function addData(): void {
 
+  // const apiKey = `${process.env.apiKey}`;
+
+
+  // const headers = new Headers();
+  // headers.append('x-api-key', apiKey);
+
+  // const controller = new AbortController();
+  // const signal = controller.signal;
+
+  // const timeout = setTimeout(() => {
+  //   controller.abort();
+  // }, 5000);
+
+  // ! 애플 데이터를 가지고 와서 
+  // fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=AAPL&apikey=${apiKey}`, {
+  //   method: 'GET',
+  //   headers: headers
+  // })
+  // .then(response => {
+  //   return response.json();
+  // })
+  // .then(data => {
+  //   const datatest = JSON.stringify(data,null,2);
+  // fs.writeFile('test.json', datatest, (error) => {
+
+  // });
+  //   console.log("이게 데이터야",data); // 여기서 데이터를 사용합니다
+  // })
+  // .catch(error => {
+  //   console.error(error);
+  // })
+  // .finally(() => {
+  //   clearTimeout(timeout);
+  // });
+
   const datatest = fs.readFileSync('./test.json', 'utf8');
   const parseData = JSON.parse(datatest);
-  let dayList : string[] = [];
+  let dayList: string[] = [];
   // console.log(parseData["Weekly Time Series"]);
   for (const key in parseData["Weekly Time Series"]) {
     dayList.push(key)
@@ -73,7 +75,7 @@ function addData(): void {
 
   // 주식 종목 회사 이름
   const stockName = parseData["Meta Data"]["2. Symbol"];
- 
+
   dbConnect.query(`create table ${stockName}(
     day DATE NOT NULL,
     open DOUBLE,
@@ -83,13 +85,13 @@ function addData(): void {
     volume DOUBLE
   );`, (err, result) => {
     console.log(result);
-    if(err){
+    if (err) {
       console.log(err)
     }
     for (let i = 0; i < dayList.length; i++) {
       const test = Object.values(parseData["Weekly Time Series"][dayList[i]])
-      dbConnect.query(`insert  INTO ${stockName}(day, open, high, low,close, volume) VALUES("${dayList[i]}",${test[0]}, ${test[1]}, ${test[2]}, ${test[3]},${test[4]});`, (err, result) =>{
-        if(err){
+      dbConnect.query(`insert  INTO ${stockName}(day, open, high, low,close, volume) VALUES("${dayList[i]}",${test[0]}, ${test[1]}, ${test[2]}, ${test[3]},${test[4]});`, (err, result) => {
+        if (err) {
           console.log(err)
         }
         // console.log("성공")
