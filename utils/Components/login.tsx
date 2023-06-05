ed2f1f860e006ffa0edf6f837e68b32855252825
wjkk21 <verstand821@gmail.com>
2023-06-05 12:36:14 +0900

    이미지 연결 중 오류 발생으로 인해 file-loader 설치

--- utils/Components/login.tsx
+++ utils/Components/login.tsx
@@ -1,10 +1,10 @@
 import React from "react";
+import "../../src/views/css/login";
 
 export default function Login() {
   return (
     <>
     <div>
-      <img src="../../src/views/img/img-13.png" alt="사람이미지" />
 
       <button type='submit'>Google을 이용하여 시작하기</button>
       <button type='submit'>회원가입</button>
