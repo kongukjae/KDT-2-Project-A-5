e0bf5debcc08b766ab457da28f2d8cd1eeb81fd8
ParkDalJae <dv.over931@gmail.com>
2023-06-07 12:59:38 +0900

    app.ts 예준님버전 app.ts 확인

--- src/controllers/app.ts
+++ src/controllers/app.ts
@@ -1,13 +1,17 @@
-import express,{Request, Response} from "express";
+import express, { Request, Response } from "express";
 import path from "path";
-import * as fs from 'fs';
+import * as fs from "fs";
 // import dotenv from "dotenv";
 import dbConnect from "../../utils/DB/dbConfigure";
 import axios from "axios";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import { Server } from "socket.io";
 import http from "http";
 import cryto from "crypto";
+// import cheerio from "cheerio";
+const cheerio = require("cheerio");
+
+const $ = cheerio.load(path.join(__dirname)); // 테스트용
 
 // import fs from "fs";
 dotenv.config({ path: "../../.env" }); // env 경로 설정
@@ -45,16 +49,15 @@ dbConnect.connect((err) => {
 //   stockDataRequest();
 app.use(express.static(root)); //root 디렉토리
 app.use(express.static(rootPublic)); //root의 하위 디렉토리는 첫번째만 접근 가능하기 때문에 별도로 지정.
-app.get('*', (req : Request, res : Response) => {
-  res.sendFile(path.join(rootPublic,"index.html"));
-})
+app.get("*", (req: Request, res: Response) => {
+  res.sendFile(path.join(rootPublic, "index.html"));
+});
 
 // 회사 명으로 테이블을 생성을 하고 데이터를 날짜 별로 튜플을 생성을 하였다.
 // addData();
 // function addData(): void {
 
 // const apiKey = `${process.env.apiKey}`;
-
 
 // const headers = new Headers();
 // headers.append('x-api-key', apiKey);
@@ -66,7 +69,7 @@ app.get('*', (req : Request, res : Response) => {
 //   controller.abort();
 // }, 5000);
 
-// ! 애플 데이터를 가지고 와서 
+// ! 애플 데이터를 가지고 와서
 // fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=AAPL&apikey=${apiKey}`, {
 //   method: 'GET',
 //   headers: headers
@@ -131,31 +134,41 @@ app.use(express.json()); // JSON 형식의 본문을 파싱할 수 있도록 설
 app.use(express.urlencoded({ extended: true })); // URL-encoded 형식의 본문을 파싱할 수 있도록 설정
 class User {
   // 타입스크립트에서 클래스의 속성을 초기화하기 위해서는 다음과 같이 클래스 내에 해당 속성을 선언하고, 생성자(Constructor)에서 초기값을 할당해야 합니다.
-  email: string
+  email: string;
   userName: string;
   _password: string;
   userphoneNum: number;
   userAccountNum: number;
-  constructor(email: string,  password: string, userName: string,userphoneNum: number, userAccountNum: number) {
+  constructor(
+    email: string,
+    password: string,
+    userName: string,
+    userphoneNum: number,
+    userAccountNum: number
+  ) {
     this.email = email;
     this._password = password;
     this.userName = userName;
     this.userphoneNum = userphoneNum;
     this.userAccountNum = userAccountNum;
   }
-  crypto = (pw : string) => {
-    this._password =  cryto.createHash("sha512").update(pw).digest("base64");
+  crypto = (pw: string) => {
+    this._password = cryto.createHash("sha512").update(pw).digest("base64");
     return this._password;
-  }
-
-
+  };
 }
 
-app.post('/creataccount', (req, res) => {
+app.post("/creataccount", (req, res) => {
   const postData = req.body; // 요청의 본문을 가져옵니다.
-  const test = new User(postData.email,postData.password,postData.name,postData.phoneNumber,123412314)
+  const test = new User(
+    postData.email,
+    postData.password,
+    postData.name,
+    postData.phoneNumber,
+    123412314
+  );
   // 비밀번호 암호화를 할 수 있도록 클래스 안에 암호화 해주는 함수를 추가 해주었다.
-  console.log('테스트 클래스 비밀번호 암호화',test.crypto(postData.password));
+  console.log("테스트 클래스 비밀번호 암호화", test.crypto(postData.password));
   console.log("데이터", req.body); // 본문의 내용을 출력하거나 원하는 작업을 수행합니다.
   // dbConnect.query(`insert INTO user_infor(userId, password, userName, phoneNum,userAccountNum) VALUES('${postData.email}','${postData.password}','${postData.name}','${postData.phoneNumber}',${123412314});`, (err, result) => {
   //   if (err) {
@@ -164,8 +177,8 @@ app.post('/creataccount', (req, res) => {
   //   console.log(result);
 
   // });
-  res.send('POST 요청이 성공적으로 처리되었습니다.');
-})
+  res.send("POST 요청이 성공적으로 처리되었습니다.");
+});
 
 app.use((req, res) => {
   res.status(404).send("not found");
