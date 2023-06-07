622c23d635088bb094c381b4438d676cd7dbf3b3
박준형 <108733795+krapli441@users.noreply.github.com>
2023-06-07 15:41:36 +0900

    가상의 주식 데이터를 생성하여 main.tsx에서 반복문으로 stockContentsBox 생성

--- utils/Components/main.tsx
+++ utils/Components/main.tsx
@@ -4,26 +4,46 @@ import ContentBox from "./stockContentsBox";
 import DriverContentBox from "./driverContentsBox";
 import NewsContentBox from "./newsContentsBox";
 
+// ? CSS
 import "../../src/views/css/Area";
 
+// ? 샘플 데이터
+import { southKoreaStock } from "../../src/models/stockdata";
+
 export default function Main() {
+  // domesticStockMarket : 국내 증시
+  let domesticStockMarket = [];
+  for (let i = 0; i < southKoreaStock.length; i++) {
+    domesticStockMarket.push(
+      <ContentBox
+        stockName={southKoreaStock[i].stockName}
+        stockPrice={southKoreaStock[i].stockPrice}
+        stockChangePercentage={southKoreaStock[i].stockChangePercentage}
+        stockChartGraph={southKoreaStock[i].stockChartGraph}
+      />
+    );
+  }
   return (
     <>
       <div className="main">
+        <h3>국내 증시</h3>
         <div className="stockArea">
-          <ContentBox
+          {/* <ContentBox
             stockName="애플"
             stockPrice="177.83 USD"
             stockChangePercentage="-1.73%"
             stockChartGraph="GRAPH"
-          />{" "}
+          />
           <ContentBox
             stockName="삼성전자"
             stockPrice="63000 KRW"
             stockChangePercentage="+2.23%"
             stockChartGraph="GRAPH"
-          />
+          /> */}
+          {domesticStockMarket}
         </div>
+        <h3>내가 자주 본 종목</h3>
+
         <div className="myStockArea">
           <ContentBox
             stockName="삼성전자"
@@ -32,6 +52,8 @@ export default function Main() {
             stockChartGraph="GRAPH"
           />
         </div>
+        <h3>모범 운전수</h3>
+
         <div className="driverArea">
           <DriverContentBox
             driverName="박준형"
@@ -46,6 +68,7 @@ export default function Main() {
             driverBadTag="급발진"
           />
         </div>
+        <h3>뉴스</h3>
         <div className="newsArea">
           <NewsContentBox
             NewsPaper="한국경제"
