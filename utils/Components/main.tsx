53bace6d6dbd139303eba384ba5efe15f5b84233
박준형 <108733795+krapli441@users.noreply.github.com>
2023-06-07 11:18:42 +0900

    컴포넌트 구조를 바꿔줘야 할 듯함

--- utils/Components/main.tsx
+++ utils/Components/main.tsx
@@ -1,15 +1,15 @@
 import React from "react";
-import ContentsBox from "./ContentsBoxArea";
+import StockContentsBox from "./stockComponents/stockContentsBoxArea";
 import "../../src/views/css/style";
 
 export default function Main() {
   return (
     <>
       <div className="main">
-        <ContentsBox h2="국내 증시" />
-        <ContentsBox h2="내가 자주 본 종목" />
-        <ContentsBox h2="모범 운전수" />
-        <ContentsBox h2="뉴스" />
+        <StockContentsBox h2="국내 증시" />
+        <StockContentsBox h2="내가 자주 본 종목" />
+        <StockContentsBox h2="모범 운전수" />
+        <StockContentsBox h2="뉴스" />
       </div>
     </>
   );
