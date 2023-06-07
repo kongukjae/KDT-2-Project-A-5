25b10462af9a75315d81c4228bfc21e0da45327f
jangrubin <wkdfnqls2465@gmail.com>
2023-06-07 13:06:45 +0900

    패키지, 모듈 등 브랜치 이동하면서 수정되어 정상 build 안됨

--- utils/Components/stockComponents/stockContentsBoxLayout.tsx
+++ utils/Components/stockComponents/stockContentsBoxLayout.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import StockDataComponent from "./stockDataComponent";
-import StockThumbnail from "./stockThumbnail";
 export default () => {
   const [showLayout, setShowLayout] = useState<JSX.Element[]>([]);
 
@@ -10,13 +9,12 @@ export default () => {
 
   // 컴포넌트를 널어 자료 화면을 띄운다.
   useEffect(() => {
-    addTagToLayout(<StockThumbnail stockName="APPLE" stockPrice="179.58 USD" stockChartGraph="Graph" stockChangePercentage="-1.37%" />);
+    <StockDataComponent></StockDataComponent>
   }, []);
 
   return (
     <>
       <div className="contentBox">{showLayout}</div>
-      <StockDataComponent></StockDataComponent>
     </>
   );
 };
