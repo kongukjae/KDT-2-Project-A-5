400443e5a2923e9b8d70e9d0af6491eb1b85e29f
박준형 <108733795+krapli441@users.noreply.github.com>
2023-06-07 14:02:10 +0900

    주식 컨텐츠 박스, 기사 컨텐츠 박스, 뉴스 컨텐츠 박스를 main 컴포넌트 안에 넣어보는 방식으로 시도.

---
+++ utils/Components/driverContentsBox.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+interface DriverContentsBoxProps {
+  driverName: string;
+  driverNoAccidentCount: string;
+  driverGoodTag: string;
+  driverBadTag: string;
+}
+
+export default function DriverContentsBox(props: DriverContentsBoxProps) {
+  const { driverName, driverNoAccidentCount, driverGoodTag, driverBadTag } =
+    props;
+
+  return (
+    <div className="driverContentsBox">
+      <div className="driverName">{driverName}</div>
+      <div className="driverNoAccidentCount">{driverNoAccidentCount}</div>
+      <div className="driverGoodTag">{driverGoodTag}</div>
+      <div className="driverBadTag">{driverBadTag}</div>
+    </div>
+  );
+}
--- utils/Components/main.tsx
+++ utils/Components/main.tsx
@@ -1,11 +1,61 @@
 import React from "react";
 import "../../src/views/css/style";
+import ContentBox from "./stockcontentsBox";
+import DriverContentsBox from "./driverContentsBox";
+import NewsContentsBox from "./newsContentsBox";
 
 export default function Main() {
   return (
     <>
       <div className="main">
-
+        <div className="stockArea">
+          <ContentBox
+            stockName="애플"
+            stockPrice="177.83 USD"
+            stockChangePercentage="-1.73%"
+            stockChartGraph="GRAPH"
+          />{" "}
+          <ContentBox
+            stockName="삼성전자"
+            stockPrice="63000 KRW"
+            stockChangePercentage="+2.23%"
+            stockChartGraph="GRAPH"
+          />
+        </div>
+        <div className="myStockArea">
+          <ContentBox
+            stockName="삼성전자"
+            stockPrice="63000 KRW"
+            stockChangePercentage="+2.23%"
+            stockChartGraph="GRAPH"
+          />
+        </div>
+        <div className="driverArea">
+          <DriverContentsBox
+            driverName="박준형"
+            driverNoAccidentCount="무사고 83일"
+            driverGoodTag="방어운전"
+            driverBadTag="급발진"
+          />
+          <DriverContentsBox
+            driverName="운전수"
+            driverNoAccidentCount="무사고 0일"
+            driverGoodTag="방어운전"
+            driverBadTag="급발진"
+          />
+        </div>
+        <div className="newsArea">
+          <NewsContentsBox
+            NewsContentsBoxNewsPaper="한국경제"
+            NewsContentsBoxThumbnail="썸네일 사진"
+            NewsContentsBoxTitleText="환율 1380원 돌파 어쩌구"
+          />
+          <NewsContentsBox
+            NewsContentsBoxNewsPaper="동아일보"
+            NewsContentsBoxThumbnail="썸네일 사진"
+            NewsContentsBoxTitleText="파월 자이언트스텝 어쩌구"
+          />
+        </div>
       </div>
     </>
   );
