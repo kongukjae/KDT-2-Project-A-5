a36447d69c126850920fb28f54afdb4fd245830e
박준형 <108733795+krapli441@users.noreply.github.com>
2023-06-07 11:53:51 +0900

    꼬인 컴포넌트들을 모두 제거. 다시 제작해보기.

--- utils/Components/main.tsx
+++ utils/Components/main.tsx
@@ -1,17 +1,11 @@
 import React from "react";
-import StockContentsBox from "./stockComponents/stockContentsBoxArea";
-import DriverContentsBox from "./driverComponents/driverContentsBoxArea";
-import NewsContentsBox from "./newsComponents/newsContentsBoxArea";
 import "../../src/views/css/style";
 
 export default function Main() {
   return (
     <>
       <div className="main">
-        <StockContentsBox h2="국내 증시" />
-        <StockContentsBox h2="내가 자주 본 종목" />
-        <DriverContentsBox h2="모범 운전수" />
-        <NewsContentsBox h2="뉴스" />
+
       </div>
     </>
   );
--- utils/Components/station.tsx
+++ utils/Components/station.tsx
@@ -1,14 +1,10 @@
 import React from "react";
-import ContentsBox from "./contentsBoxArea";
 import "../../src/views/css/style";
 
 export default function Main() {
   return (
     <>
       <div className="main">
-        <ContentsBox h2="정류장" />
-        <ContentsBox h2="포르쉐" />
-        <ContentsBox h2="람보르기니" />
       </div>
     </>
   );
