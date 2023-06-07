3b811dec9b5749e0a0e5ccb4419c771e3e5252d0
KwakYounHo <kwakyh951@gmail.com>
2023-06-08 00:33:05 +0900

    뉴스데이터 받아와서 컴포넌트에 띄우기 app_kwak.ts : 뉴스 API생성 newsApiParse.tsx : 뉴스 데이터 출력 컴포넌트 station.tsx : 정류장 페이지에 임시로 띄움 newsApi.ts : 뉴스 데이터 요청하는 함수 모듈 제작

--- utils/naverNewsApi/newsApi.ts
+++ utils/naverNewsApi/newsApi.ts
@@ -7,18 +7,18 @@ const client_secret = `${process.env.naverDevClientSec}`;
 const query = "경제";
 const displayLength = `3`;
 const category = 101;
+const api_url = `https://openapi.naver.com/v1/search/news?query=${encodeURIComponent(
+  query
+)}&display=${displayLength}&category=${category}`;
 
 const data = async () => {
-  const api_url = `https://openapi.naver.com/v1/search/news?query=${encodeURIComponent(
-    query
-  )}&display=${displayLength}&category=${category}`;
   const request = await axios.get(api_url, {
     headers: {
       "X-Naver-Client-Id": `${client_id}`,
       "X-Naver-Client-Secret": `${client_secret}`,
     },
   });
-  console.log(await request.data.items);
+  return request;
 };
 
-export default data();
+export default data;
