80f19042efcb28d6bf546144daf6e47cec03ad2a
ParkDalJae <dv.over931@gmail.com>
2023-06-08 11:26:03 +0900

    newsApiParse.tsx 파일 HTMl 특수코드자동출력부분 DOMParser로 해결

--- utils/Components/newsApiParse.tsx
+++ utils/Components/newsApiParse.tsx
@@ -3,20 +3,34 @@ import axios from "axios";
 
 const newsAPI = (): JSX.Element => {
   const [news, setNews] = useState([]);
-  useEffect(()=>{
-    const requestNews = async() => {
-      const request = await axios.get('/news');
+  useEffect(() => {
+    const requestNews = async () => {
+      const request = await axios.get("/news");
       const result = request.data.items;
       console.log(result);
       setNews(result);
-    }
+    };
     requestNews();
-  }, [])
+  }, []);
+
+  const decodeHtmlEntitles = (text: string) => {
+    const parse = new DOMParser();
+    const decodedString = parse.parseFromString(text, "text/html").body
+      .textContent;
+    return decodedString;
+  };
+
   return (
     <div>
-      {(news.length > 0)? news.map((element:{[key:string]:string})=>{return <div>{element.title}</div>}) : <div>데이터 준비중 'ㅡ' /</div>}
+      {news.length > 0 ? (
+        news.map((element: { [key: string]: string }) => {
+          return <div>{decodeHtmlEntitles(element.title)}</div>;
+        })
+      ) : (
+        <div>데이터 준비중 'ㅡ' /</div>
+      )}
     </div>
-  )
+  );
 };
 
 export default newsAPI;
