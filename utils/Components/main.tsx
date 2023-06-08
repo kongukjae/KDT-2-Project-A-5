035c2e251b9351d579a9ce9ff34f1936cefb227c
박준형 <108733795+krapli441@users.noreply.github.com>
2023-06-08 14:02:03 +0900

    main.tsx : DriverData를 불러오는 위치를 변경. index.bundle.js : 테스트 하기 위해 번들링한 파일.

--- utils/Components/main.tsx
+++ utils/Components/main.tsx
@@ -3,7 +3,6 @@ import "../../src/views/css/style";
 import StockData from "./stockContentsBox";
 import DriverContentBox from "./driverContentsBox";
 import NewsContentBox from "./newsContentsBox";
-import { DriverData } from "./driver";
 
 // ? CSS
 import "../../src/views/css/Area";
@@ -13,64 +12,24 @@ import "../../src/views/css/Area";
 import { southKoreaStock } from "../../src/models/stockdata";
 import { bestDriverData } from "../../src/models/driverdata";
 import { newsData } from "../../src/models/newsData";
+import { DriverData } from "./driver";
 
 // ! 데이터
 import NewsAPI from "./newsApiParse";
 
 export default function Main() {
-  // ? 주식 데이터를 넣는 비어있는 배열
-  // ? domesticStockMarket : 국내 증시
-  // let domesticStockMarket = [];
-
-  // // ? 가상으로 만들어진 주식 데이터의 length만큼 반복문 실행
-  // for (let i = 0; i < southKoreaStock.length; i++) {
-  //   domesticStockMarket.push(
-  //     <StockContentBox
-  //       stockName={southKoreaStock[i].stockName}
-  //       stockPrice={southKoreaStock[i].stockPrice}
-  //       stockChangePercentage={southKoreaStock[i].stockChangePercentage}
-  //       stockChartGraph={southKoreaStock[i].stockChartGraph}
-  //       key={southKoreaStock[i].id}
-  //     />
-  //   );
-  // }
-
-  // ? 모범 운전수 데이터를 넣는 비어있는 비열
-  let bestDriverDataBox = [];
-
-  for (let i = 0; i < bestDriverData.length; i++) {
-    bestDriverDataBox.push(
-      <DriverContentBox
-        driverName={bestDriverData[i].driverName}
-        driverNoAccidentCount={bestDriverData[i].driverNoAccidentCount}
-        driverGoodTag={bestDriverData[i].driverGoodTag}
-        driverBadTag={bestDriverData[i].driverBadTag}
-      />
-    );
-  }
-
-  let newsDataBox = [];
-
-  for (let i = 0; i < newsData.length; i++) {
-    newsDataBox.push(
-      <NewsContentBox
-        NewsPaper={newsData[i].NewsPaper}
-        NewsThumbnail={newsData[i].NewsThumbnail}
-        NewsTitleText={newsData[i].NewsTitleText}
-      />
-    );
-  }
   let driverArea = [];
 
   for (let i = 0; i < DriverData.length && i < 5; i++) {
     const driver = DriverData[i];
     driverArea.push(
-      <DriverContentsBox
+      <DriverContentBox
         driverName={driver.driverName}
         driverOperationsCount={""}
         driverNoAccidentCount={driver.driverNoAccidentCount}
         driverGoodTag={driver.driverGoodTag}
-        driverBadTag={driver.driverBadTag}       />
+        driverBadTag={driver.driverBadTag}
+      />
     );
   }
 
@@ -86,9 +45,7 @@ export default function Main() {
           <StockData />
         </div>
         <h3>모범 운전수</h3>
-
         <div className="driverArea">{driverArea}</div>
-
         <h3>뉴스</h3>
         <div className="newsArea">
           {/* {newsDataBox} */}
