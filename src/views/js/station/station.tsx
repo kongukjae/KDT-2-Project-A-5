cae22b54c2756934bc382f1ec91a1a1d03949682
KwakYounHo <kwakyh951@gmail.com>
2023-06-08 09:30:45 +0900

    버그 수정 : 기사 두 번 나오는 것

--- src/views/js/station/station.tsx
+++ src/views/js/station/station.tsx
@@ -1,12 +1,10 @@
 import React, { useState } from "react";
 import Station from "../../../../utils/Components/station";
-import News from "../../../../utils/Components/newsApiParse";
 
 const Screen: React.FC = () => {
   return (
     <>
       <Station />
-      <News />
     </>
   );
 };
