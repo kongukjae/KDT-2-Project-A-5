f3fc2f824e591549aaa26b62e290422da2344c18
jangrubin <wkdfnqls2465@gmail.com>
2023-06-09 12:43:52 +0900

    코드 수정 = 컨텍스트로 받아온 데이터 useState에 담음

--- utils/Components/stockContentsBox.tsx
+++ utils/Components/stockContentsBox.tsx
@@ -3,6 +3,7 @@ import stockContext, { stockContextType } from "../../src/views/js/stockContext"
 
 const StockData = (): JSX.Element => {
   let stocktest = useContext<stockContextType | null>(stockContext);
+  console.log(stocktest);
   // 최초에 값 받아올 때 에러를 피하기 위해 더미데이터
   const [contextData, setContextData] = useState<any>({
     symbol: '잠시만 기다려주세요',
@@ -12,11 +13,11 @@ const StockData = (): JSX.Element => {
       '2023-06-05': { open: 0, high: 0, low: 0, close: 0 },
     },
   });
-
+  let price;
 // 회사 데이터
   useEffect(() => {
     if (stocktest) {
-      setContextData(stocktest.symbol);
+      setContextData(stocktest);
     }
     // 테스트 하기 위해 랜더링 될 때마다 재실행
   }, [stocktest]);
@@ -24,17 +25,23 @@ const StockData = (): JSX.Element => {
   useEffect(()=> {
     if(stocktest) {
       // 주식 데이터(객체) -> 배열로 변환
-      const priceArray = Object.entries(stocktest?.price).map(([date, price]) => {
+      const priceArray : any = Object.entries(stocktest?.price).map(([date, price]) => {
         return { date, ...price };
       });
-      console.log("컨텍스트의 주가 데이터 = 배열",priceArray[0]);
+      console.log("컨텍스트의 주가 데이터 = 배열",priceArray[0]['1. open']);
+      price = priceArray[0]['1. open'];
+      // for(let i=0; i<priceArray.length; i++) {
+      //   price = priceArray[0]['1. open'];
+      // };
     }
-  }, [stocktest])
+    
+  }, [setContextData])
 
   return (
     <>
       <div>
-        {stocktest?.symbol}
+        {contextData?.symbol}
+        {price}
       </div>
     </>
   );
