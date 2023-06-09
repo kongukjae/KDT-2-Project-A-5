3fc09e8bcb46286dd4faa89f9d7e6c5895fae22f
박준형 <108733795+krapli441@users.noreply.github.com>
2023-06-09 11:14:16 +0900

    가상의 주식 데이터를 활용하여 Object.entries()를 사용해 객체를 배열로 바꿔보았음.

---
+++ src/models/virtualstockdata.ts
@@ -0,0 +1,67 @@
+import data from "../../utils/naverNewsApi/newsApi";
+
+const price = {
+  "2023.06.05": {
+    open: 0,
+    high: 0,
+    low: 0,
+    close: 0,
+  },
+  "2023.06.06": {
+    open: 0,
+    high: 0,
+    low: 0,
+    close: 0,
+  },
+  "2023.06.7": {
+    open: 0,
+    high: 0,
+    low: 0,
+    close: 0,
+  },
+  "2023.06.08": {
+    open: 0,
+    high: 0,
+    low: 0,
+    close: 0,
+  },
+  "2023.06.09": {
+    open: 0,
+    high: 0,
+    low: 0,
+    close: 0,
+  },
+  "2023.06.10": {
+    open: 0,
+    high: 0,
+    low: 0,
+    close: 0,
+  },
+  "2023.06.11": {
+    open: 0,
+    high: 0,
+    low: 0,
+    close: 0,
+  },
+  "2023.06.12": {
+    open: 0,
+    high: 0,
+    low: 0,
+    close: 0,
+  },
+};
+
+const priceArray: {
+  date: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+}[] = Object.entries(price).map(([date, data]) => {
+  return {
+    date,
+    ...data,
+  };
+});
+
+console.log(priceArray);
