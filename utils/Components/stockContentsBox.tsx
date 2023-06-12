bec80f847e9708f7d40b55143c32bf54853bc958
박준형 <108733795+krapli441@users.noreply.github.com>
2023-06-12 14:23:46 +0900

    stockContentsBox : 차트에 대한 속성값들 변경. x축, y축 선을 삭제함.

--- utils/Components/stockContentsBox.tsx
+++ utils/Components/stockContentsBox.tsx
@@ -1,31 +1,44 @@
 import React, { useContext, useEffect, useState } from "react";
-import { CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
-import stockContext, { stockContextType } from "../../src/views/js/stockContext";
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  Tooltip,
+  XAxis,
+  YAxis,
+  ResponsiveContainer,
+} from "recharts";
+import stockContext, {
+  stockContextType,
+} from "../../src/views/js/stockContext";
 
 const StockData = (): JSX.Element => {
   const stocktest = useContext<stockContextType | null>(stockContext);
   const [contextData, setContextData] = useState<any>({
-    symbol: '잠시만 기다려주세요',
+    symbol: "잠시만 기다려주세요",
     price: {
-      '2023-06-07': { open: 0, high: 0, low: 0, close: 0 },
-      '2023-06-06': { open: 0, high: 0, low: 0, close: 0 },
-      '2023-06-05': { open: 0, high: 0, low: 0, close: 0 },
+      "2023-06-07": { open: 0, high: 0, low: 0, close: 0 },
+      "2023-06-06": { open: 0, high: 0, low: 0, close: 0 },
+      "2023-06-05": { open: 0, high: 0, low: 0, close: 0 },
     },
   });
   const [test, setTest] = useState<any[]>([]);
 
   // useEffect(() => {
   //   if (stocktest) {
-      
+
   //   }
   // }, [stocktest]);
 
   useEffect(() => {
     if (stocktest) {
       setContextData(stocktest);
-      const priceArray: any[] = Object.entries(stocktest?.price).map(([date, price]) => {
-        return { date, ...price };
-      });
+      const priceArray: any[] = Object.entries(stocktest?.price).map(
+        ([date, price]) => {
+          return { date, ...price };
+        }
+      );
       console.log("컨텍스트의 주가 데이터 = 배열로 변환한 것 : ", priceArray);
 
       let intervalNumber = 0;
@@ -44,14 +57,29 @@ const StockData = (): JSX.Element => {
 
   const SimpleLineChart = () => {
     return (
-      <LineChart width={500} height={300} data={test}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="1. open" />
-        <YAxis type="number" domain={[0, 150000]} tickFormatter={(value) => `${value}원`}/>
-        <Tooltip />
-        <Legend />
-        <Line type="monotone" dataKey="1. open" stroke="#E63946" />
-      </LineChart>
+      <div className="stockChart">
+        <h4></h4>
+        {/* <ResponsiveContainer width={110} height={40}> */}
+        <LineChart width={110} height={40} data={test}>
+          {/* <CartesianGrid strokeDasharray="3 3" /> */}
+          {/* <XAxis dataKey="date" /> */}
+          <YAxis
+            hide={true}
+            // type="number"
+            domain={["auto", "auto"]}
+            // tickFormatter={(value) => `${value}USD`}
+          />
+          {/* <Tooltip /> */}
+          {/* <Legend /> */}
+          <Line
+            type="monotone"
+            dataKey="1. open"
+            stroke="#E63946"
+            dot={false}
+          />
+        </LineChart>
+        {/* </ResponsiveContainer> */}
+      </div>
     );
   };
   // const renderLineChart = ()=> {
