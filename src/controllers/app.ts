d4dd8e26b148cdb58481298812f36bb3af43f075
jangrubin <wkdfnqls2465@gmail.com>
2023-06-12 14:27:24 +0900

    서버측 주식 데이터 요청 부분 이동 및 수정, 클라이언트 측 수정 필요

--- src/controllers/app.ts
+++ src/controllers/app.ts
@@ -12,30 +12,34 @@ const rootPublic = path.join(root, "public"); //C:\Users\over9\KDT-2_FullStack\K
 const app = express();
 const socketServer = http.createServer(app);
 const io = new Server(socketServer);
-//최초 주식 데이터
+//! 최초 주식 데이터 요청 함수
 let stockData: any = null;
+let jsonData : string;
+async function stockDataRequest() {
+  try {
+    const symbol = "IBM";
+    const apiKey = process.env.alphaApiKey;
+    const response = await axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=5min&apikey=${apiKey}`)
+    stockData = response.data;
+    // console.log(stockData);
+    //api로 받아온 데이터 json으로 전송
+    jsonData = JSON.stringify(stockData);
+    // console.log(jsonData);
+    // io.emit("stockDataUpdate", jsonData);
+  } catch (error) {
+    console.error('주식 데이터를 받아오는데 실패했습니다', error);
+  }
+  // 서버에서 3분에 한번씩 주식데이터 요청
+  setTimeout(stockDataRequest, 3 * 60 * 1000);
+}
+stockDataRequest();
+// 최초 주식 데이터 요청
+
 io.on("connect", (socket) => {
   console.log("소켓에 최초 연결 됐습니다 - 서버");
+  // 주식 데이터 클라이언트에 전송
+  socket.emit("stockDataUpdate", jsonData);
   // 알파벤티지에 주식 데이터 요청하는 함수
-  async function stockDataRequest() {
-    try {
-      const symbol = "IBM";
-      const apiKey = process.env.alphaApiKey;
-      const response = await axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=5min&apikey=${apiKey}`)
-      stockData = response.data;
-      // console.log(stockData);
-      //api로 받아온 데이터 json으로 전송
-      let jsonData = JSON.stringify(stockData);
-      // console.log(jsonData);
-      socket.emit("stockDataUpdate", jsonData);
-    } catch (error) {
-      console.error('주식 데이터를 받아오는데 실패했습니다', error);
-    }
-    // 3분에 한번씩 주식데이터 요청
-    setTimeout(stockDataRequest, 1 * 60 * 1000);
-  }
-  // 최초 주식 데이터 요청
-  stockDataRequest();
   // 소켓 연결 해제
   socket.on("disconnect", () => {
     console.log("소켓에 연결 해제됐습니다 - 서버");
