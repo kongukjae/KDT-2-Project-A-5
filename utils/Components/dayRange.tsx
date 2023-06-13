91f2371bef985683572989ba4c9a84f40b7e1481
jangrubin <wkdfnqls2465@gmail.com>
2023-06-13 12:14:06 +0900

    dayRange 컴포넌트에서 useContext하여 주식 데이터 활용하려 함(중간 저장)

--- utils/Components/dayRange.tsx
+++ utils/Components/dayRange.tsx
@@ -1,11 +1,11 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import stockContext, {
   stockContextType
 } from "../../src/views/js/stockContext";
 const DayRange = (): JSX.Element => {
   const dayRangeContext = useContext<stockContextType | null>(stockContext);
-  console.log(dayRangeContext)
-  const [contextData, setContextData] = useState<any>({
+  // console.log(dayRangeContext)
+  const [dayRange, setDayRange] = useState<any>({
     symbol: "잠시만 기다려주세요",
     price: {
       "2023-06-07": { open: 0, high: 0, low: 0, close: 0 },
@@ -13,7 +13,34 @@ const DayRange = (): JSX.Element => {
       "2023-06-05": { open: 0, high: 0, low: 0, close: 0 },
     },
   });
-  return (<div></div>)
+  const [test, setTest] = useState<any[]>([]);
+
+  useEffect(() => {
+    // 컨텍스트 데이터를 모두 가져와서 시간별 객체 데이터를 배열에 담아줌
+    if (dayRangeContext) {
+      setDayRange(dayRangeContext);
+      const priceArray: any[] = Object.entries(dayRangeContext?.price).map(
+        ([date, price]) => {
+          return { date, ...price };
+        }
+      );
+      let intervalNumber = 0;
+      let interval = setInterval(() => {
+        let lastPrice = priceArray[intervalNumber];
+        setTest((prevTest) => [...prevTest, lastPrice]);
+        intervalNumber++;
+        // 배열 길이 넘어가면 interval 중지
+        if (intervalNumber >= priceArray.length) {
+          clearInterval(interval);
+        }
+      }, 5000);
+    }
+    console.log(test);
+  }, [dayRangeContext]);
+
+  return (<div>
+    {/* {dayRange} */}
+  </div>)
 }
   // const [test, setTest] = useState<any[]>([]);
 export default DayRange;
