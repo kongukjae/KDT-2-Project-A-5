4233fecb110a24343c29362ffeb53822902df6bb
ParkDalJae <dv.over931@gmail.com>
2023-06-13 15:33:25 +0900

    taxiPage 폴더 생성 taxiScreen.tsx 생성 taxi.tsx 생성

---
+++ src/views/js/taxiPage/taxiScreen.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import Station from "../../../../utils/Components/station";
+
+const Screen: React.FC = () => {
+  return (
+    <>
+      <Station />
+    </>
+  );
+};
+
+export default Screen;
