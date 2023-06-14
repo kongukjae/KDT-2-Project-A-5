0a5956dbcd9bee706a6d8612b2a7a10316290049
ParkDalJae <dv.over931@gmail.com>
2023-06-14 16:58:06 +0900

    stockSearch.css 부분 import 했음

--- utils/Components/stockSearch.tsx
+++ utils/Components/stockSearch.tsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { ChangeEvent } from "react";
+import "../../src/views/css/stockSearch.css"
+
 
 export default function companySearch() {
   const [input, setUserInput] = useState("");
@@ -20,22 +22,44 @@ export default function companySearch() {
     const filteredCompanies = companies.filter((company) => {
       return company[0].includes(input) || company[1].includes(input);
     });
+
     return filteredCompanies.map((company) => (
-      <div key={company[1]}>
+      <div
+        key={company[1]}
+        className="companyItem"
+        onMouseOver={() => handleMouseOver(company[1])}
+        onMouseOut={() => handleMouseOut(company[1])}
+      >
         {company[0]} : {company[1]}
       </div>
     ));
   };
 
+  const handleMouseOver = (companyId: string) => {
+    const element = document.getElementById(companyId);
+    if (element) {
+      element.classList.add("hovered");
+    }
+  };
+
+  const handleMouseOut = (companyId: string) => {
+    const element = document.getElementById(companyId);
+    if (element) {
+      element.classList.remove("hovered");
+    }
+  };
+
   return (
     <div className="main">
-      <input
-        onInput={getValue}
-        placeholder="종목명 혹은 종목번호를 입력해주세요."
-      />
-      {input && resultCompanie().length > 0 && (
-        <div className="stockData">{resultCompanie()}</div>
-      )}
+      <div className="mainInner">
+        <input
+          onInput={getValue}
+          placeholder="종목명 혹은 종목번호를 입력해주세요."
+        />
+        {input && resultCompanie().length > 0 && (
+          <div className="stockData">{resultCompanie()}</div>
+        )}
+      </div>
     </div>
   );
 }
