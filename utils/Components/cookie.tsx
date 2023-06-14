aca137cbb51c84cb12ca3c8bfb854a1d3cb33507
jangrubin <wkdfnqls2465@gmail.com>
2023-06-14 15:29:25 +0900

    쿠키 클래스 -> get, set 타입 지정 및 모듈 export

--- utils/Components/cookie.tsx
+++ utils/Components/cookie.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
-// import {useCookies} from 'react-cookie'
-
-const TestCookie =  ():JSX.Element => {
-  return <></>
-}
-export default TestCookie;
+import { Cookies } from 'react-cookie';
+  const cookies = new Cookies();
+  // set
+  export const setCookie = (name : string, value : string , option? : any ) => {
+    return cookies.set(name, value, { ...option })
+  }
+  // get
+  export const getCookie = (name : string)=> {
+    return cookies.get(name)
+  }
