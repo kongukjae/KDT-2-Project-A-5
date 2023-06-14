3a6507e0cab7cc8b4b8e17b55b8273148b7184f2
ParkDalJae <dv.over931@gmail.com>
2023-06-14 17:40:36 +0900

    document 오답 삭제. react

--- utils/Components/stockSearch.tsx
+++ utils/Components/stockSearch.tsx
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from "react";
 import { ChangeEvent } from "react";
 import "../../src/views/css/stockSearch.css"
 
-
 export default function companySearch() {
-  const [input, setUserInput] = useState("");
+  const [input, setInput] = useState("");
   const [companies, setCompanies] = useState<[string, string][]>([]);
+
   useEffect(() => {
     fetch("../../src/models/stock.data.json")
       .then((response) => response.json())
@@ -14,50 +14,43 @@ export default function companySearch() {
       });
   }, []);
 
-  const getValue = (e: ChangeEvent<HTMLInputElement>) => {
-    setUserInput(e.target.value);
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
   };
 
-  const resultCompanie = () => {
-    const filteredCompanies = companies.filter((company) => {
-      return company[0].includes(input) || company[1].includes(input);
-    });
-
-    return filteredCompanies.map((company) => (
-      <div
-        key={company[1]}
-        className="companyItem"
-        onMouseOver={() => handleMouseOver(company[1])}
-        onMouseOut={() => handleMouseOut(company[1])}
-      >
-        {company[0]} : {company[1]}
-      </div>
-    ));
+  const handleMouseOver = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.currentTarget.classList.add("hovered");
   };
 
-  const handleMouseOver = (companyId: string) => {
-    const element = document.getElementById(companyId);
-    if (element) {
-      element.classList.add("hovered");
-    }
+  const handleMouseOut = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.currentTarget.classList.remove("hovered");
   };
 
-  const handleMouseOut = (companyId: string) => {
-    const element = document.getElementById(companyId);
-    if (element) {
-      element.classList.remove("hovered");
-    }
-  };
+  const filteredCompanies = companies.filter(
+    (company) =>
+      company[0].includes(input) || company[1].includes(input)
+  );
 
   return (
     <div className="main">
       <div className="mainInner">
         <input
-          onInput={getValue}
+          onChange={handleChange}
           placeholder="종목명 혹은 종목번호를 입력해주세요."
         />
-        {input && resultCompanie().length > 0 && (
-          <div className="stockData">{resultCompanie()}</div>
+        {input && filteredCompanies.length > 0 && (
+          <div className="stockData">
+            {filteredCompanies.map((company) => (
+              <div
+                key={company[1]}
+                className="companyItem"
+                onMouseOver={handleMouseOver}
+                onMouseOut={handleMouseOut}
+              >
+                {company[0]} : {company[1]}
+              </div>
+            ))}
+          </div>
         )}
       </div>
     </div>
