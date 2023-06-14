51aa65c51683aa91169e08eae148c932d56b1f18
wjkk21 <verstand821@gmail.com>
2023-06-14 16:41:49 +0900

    입금 버튼 누르면 모달창 구현 희망 중 계좌 기본 금액 수정

--- utils/Components/bankAccount.tsx
+++ utils/Components/bankAccount.tsx
@@ -1,16 +1,13 @@
 import React, { useState } from "react";
-import { Router, Routes, Route } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { Router, Routes, Route, Link, useNavigate } from "react-router-dom";
 import Deposit from "./deposit";
 
 import "../../src/views/css/style";
 
-function addCommasToNumber(number: number): string {
-  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
-
 export default function Account() {
-  const [balance, setBalance] = useState(0);
+  const [balance, setBalance] = useState(999999999);
+  const [showModal, setShowModal] = useState(false);
+  const navigate = useNavigate();
 
   // 계좌에 숫자가 올라갈 때마다 호출되는 함수
   const handleDeposit = (amount: number) => {
@@ -18,14 +15,36 @@ export default function Account() {
     setBalance(newBalance);
   };
 
+  const handleModalOpen = () => {
+    setShowModal(true);
+  };
+
+  const handleModalClose = () => {
+    setShowModal(false);
+  };
+
+  const handleWithdraw = () => {
+    navigate("/withdraw");
+    handleModalClose();
+  };
+
   return (
     <div>
-      <p className="myAccount">계좌 잔액: {addCommasToNumber(balance)}</p>
-      <Link to="/deposit">
-        <button>충전</button>
-        <button>입금</button>
-      </Link>
+      <p className="myAccount">계좌 잔액: {balance.toLocaleString()}</p>
+      <button onClick={handleModalOpen}>충전</button>
+      <button onClick={handleModalOpen}>입금</button>
       {/* 추가적인 버튼 또는 로직을 여기에 추가할 수 있습니다 */}
+      {showModal && (
+        <div className="modal">
+          <div className="modal-content">
+            <button className="close-btn" onClick={handleModalClose}>
+              X
+            </button>
+            <p>입출금 화면</p>
+            <button onClick={handleWithdraw}>출금</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
