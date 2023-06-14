4d27fe9ac9b1ac5f75e0fdee7fcbabcb6f8797e7
jangrubin <wkdfnqls2465@gmail.com>
2023-06-14 12:21:30 +0900

    컨텐츠 박스 충돌 수정

--- utils/Components/stockContentsBox.tsx
+++ utils/Components/stockContentsBox.tsx
@@ -14,45 +14,6 @@ const StockData = (): JSX.Element => {
     let openPrice = socketStockData[1][1];
     priceArray.push(openPrice)
     console.log(priceArray)
-
-
-
-    // chartArray.push(socketStockData[1][1]['1. open'])
-  
-
-  // console.log(chartArray)
-  // const stocktest = useContext<stockContextType | null>(stockContext);
-  // const [contextData, setContextData] = useState<any>({
-  //   symbol: "잠시만 기다려주세요",
-  //   price: {
-  //     "2023-06-07": { open: 0, high: 0, low: 0, close: 0 },
-  //     "2023-06-06": { open: 0, high: 0, low: 0, close: 0 },
-  //     "2023-06-05": { open: 0, high: 0, low: 0, close: 0 },
-  //   },
-  // });
-  // const [test, setTest] = useState<any[]>([]);
-
-  // useEffect(() => {
-  //   if (stocktest) {
-  //     setContextData(stocktest);
-  //     const priceArray: any[] = Object.entries(stocktest?.price).map(
-  //       ([date, price]) => {
-  //         return { date, ...price };
-  //       }
-  //     );
-  //     let intervalNumber = 0;
-  //     let interval = setInterval(() => {
-  //       let lastPrice = priceArray[intervalNumber];
-  //       setTest((prevTest) => [...prevTest, lastPrice]);
-  //       intervalNumber++;
-
-  //       if (intervalNumber >= priceArray.length) {
-  //         clearInterval(interval);
-  //       }
-  //     }, 5000);
-  //   }
-  // }, [stocktest]);
-
   const SimpleLineChart = () => {
     return (
       <div className="stockChart">
@@ -71,83 +32,10 @@ const StockData = (): JSX.Element => {
       </div>
     );
   };
-
-
   return (
     <div>
-      {/* {contextData?.symbol} */}
       {/* 배열에 데이터가 추가될 때만 차트가 렌더링 */}
       {<SimpleLineChart />}
     </div>
   );
 };
-
-=======
-import React, { useContext } from "react";
-import stockContext from "../../src/views/js/stockContext";
-
-const StockData = (): JSX.Element => {
-  const socketStockData = useContext<any>(stockContext);
-  console.log("")
-  // const stocktest = useContext<stockContextType | null>(stockContext);
-  // const [contextData, setContextData] = useState<any>({
-  //   symbol: "잠시만 기다려주세요",
-  //   price: {
-  //     "2023-06-07": { open: 0, high: 0, low: 0, close: 0 },
-  //     "2023-06-06": { open: 0, high: 0, low: 0, close: 0 },
-  //     "2023-06-05": { open: 0, high: 0, low: 0, close: 0 },
-  //   },
-  // });
-  // const [test, setTest] = useState<any[]>([]);
-
-  // useEffect(() => {
-  //   if (stocktest) {
-  //     setContextData(stocktest);
-  //     const priceArray: any[] = Object.entries(stocktest?.price).map(
-  //       ([date, price]) => {
-  //         return { date, ...price };
-  //       }
-  //     );
-  //     let intervalNumber = 0;
-  //     let interval = setInterval(() => {
-  //       let lastPrice = priceArray[intervalNumber];
-  //       setTest((prevTest) => [...prevTest, lastPrice]);
-  //       intervalNumber++;
-
-  //       if (intervalNumber >= priceArray.length) {
-  //         clearInterval(interval);
-  //       }
-  //     }, 5000);
-  //   }
-  // }, [stocktest]);
-
-  // const SimpleLineChart = () => {
-  //   return (
-  //     <div className="stockChart">
-  //       <ResponsiveContainer width={110} height={80}>
-  //         <LineChart width={110} height={40} data={test}>
-  //           <YAxis hide={true} domain={["auto", "auto"]} />
-  //           <Line
-  //             type="monotone"
-  //             dataKey="1. open"
-  //             stroke="#E63946"
-  //             dot={false}
-  //           />
-  //         </LineChart>
-  //       </ResponsiveContainer>
-  //     </div>
-  //   );
-  // };
-
-
-  return (
-    <div>
-      {/* {contextData?.symbol} */}
-      {/* 배열에 데이터가 추가될 때만 차트가 렌더링 */}
-      {/* {test.length > 0 && <SimpleLineChart />} */}
-    </div>
-  );
-};
-
->>>>>>> dfdebd03283c951993b6d4fb31e4852fa4a686ce
-export default StockData;
