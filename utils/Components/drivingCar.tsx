409e1275e0bbf0dc8b0c6d220371cac2fe73e999
wjkk21 <verstand821@gmail.com>
2023-06-15 15:35:43 +0900

    drivinfCar.tsx 내용 추가 서버 요청

--- utils/Components/drivingCar.tsx
+++ utils/Components/drivingCar.tsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { getCookie } from "./cookie";
 
-const drivingCar = () => {
+const DrivingCar: React.FC = () => {
+  const cookieValue = getCookie("userName");
+  fetch("/drivingCar", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(cookieValue),
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("post 요청이 실패했습니다.");
+      }
+    })
+    .then((data) => {})
+    .catch((error) => {
+      console.error(error);
+    });
 
-  
-}
+  return <></>;
+};
 
-export default drivingCar;
+export default DrivingCar;
