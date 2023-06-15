b8f1ce7289532846e3a8a589db9148a68ec0a702
ParkDalJae <dv.over931@gmail.com>
2023-06-15 15:13:06 +0900

    form 내부 div 로 감싸서 justify 부여 텍스트 추가

--- utils/Components/taxi.tsx
+++ utils/Components/taxi.tsx
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 import Modal from "react-modal";
 import { getCookie } from "./cookie";
 
+Modal.setAppElement("#root");
+
 const corpAutoComp = () => {
   const [userName, setUserName] = useState("");
   const [stocks, setStocks] = useState("");
@@ -107,88 +109,118 @@ const corpAutoComp = () => {
   return (
     <div className="main">
       <form action="" method="" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="기사이름"
-          name="userName"
-          value={userName}
-          readOnly
-        />
-        <input
-          type="text"
-          placeholder="종목을 검색해주세요"
-          name="stocks"
-          value={stocks}
-          onChange={handleChangeStocks}
-          ref={inputStock}
-          readOnly
-        ></input>
+        <div className="flexBetween">
+          <input
+            type="hidden"
+            placeholder="기사이름"
+            name="userName"
+            value={userName}
+            readOnly
+          />
+        </div>
+        <div className="flexBetween">
+          종목 이름
+          <div>
+            <input
+              type="text"
+              placeholder="종목을 검색해주세요"
+              name="stocks"
+              value={stocks}
+              onChange={handleChangeStocks}
+              onClick={handleOpenModal}
+              ref={inputStock}
+              readOnly
+            ></input>
+            <a href="#" onClick={handleOpenModal}>
+              <button type="button" value="클릭">
+                검색
+              </button>
+            </a>
+          </div>
+        </div>
 
-        <a href="#" onClick={handleOpenModal}>
-          <button type="button" value="클릭">
-            검색
-          </button>
-        </a>
-
-        <Modal isOpen={modalIsOpen} onRequestClose={handleCloseModal}>
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={handleCloseModal}
+        >
           {/* 모달창 띄우기 */}
-          <h2>모달 제목</h2>
-          <p>모달 내용</p>
+          <h2>원하는 종목 혹은 코드를 입력해주세요</h2>
           <button onClick={handleCloseModal}>돌아가기</button>
           <StockSearch setStocks={setStocks} closeModal={handleCloseModal} />
           {/* 기존에 생성했던 컴포넌트*/}
         </Modal>
-        <input
-          type="date"
-          placeholder="모집 마감 기간"
-          name="recruitmentPeriod"
-          value={recruitmentPeriod}
-          onChange={handleChangeRecruitmentPeriod}
-        ></input>
-        <input
-          type="date"
-          placeholder="최대 운행 기간"
-          name="maximumDrivePeriod"
-          value={maximumDrivePeriod}
-          onChange={handleChangeMaximumDrivePeriod}
-        ></input>
-        <input
-          type="number"
-          placeholder="구매 희망 금액"
-          name="purchasePrice"
-          value={purchasePrice}
-          onChange={handleChangePurchasePrice}
-        ></input>
-        <input
-          type="number"
-          placeholder="구매 희망 수량"
-          name="stockAmount"
-          value={stockAmount}
-          onChange={handleChangeStockAmount}
-        ></input>
-        <input
-          type="number"
-          placeholder="희망 목표 금액"
-          name="targetPrice"
-          value={targetPrice}
-          onChange={handleChangeTargetPrice}
-        ></input>
-        <input
-          type="number"
-          placeholder="절망 폭파 금액"
-          name="stopLossPrice"
-          value={stopLossPrice}
-          onChange={handleChangeStopLossPrice}
-        ></input>
-        <input
-          type="number"
-          placeholder="최대 인원수"
-          name="maxPerson"
-          min="1"
-          max="10"
-          value={maxPerson}
-          onChange={handleChangeMaxPerson}
-        ></input>
+        <div className="flexBetween">
+          모집 마감 기간
+          <input
+            type="date"
+            placeholder="모집 마감 기간"
+            name="recruitmentPeriod"
+            value={recruitmentPeriod}
+            onChange={handleChangeRecruitmentPeriod}
+          ></input>
+        </div>
+        <div className="flexBetween">
+          최대 운행 기간
+          <input
+            type="date"
+            placeholder="최대 운행 기간"
+            name="maximumDrivePeriod"
+            value={maximumDrivePeriod}
+            onChange={handleChangeMaximumDrivePeriod}
+          ></input>
+        </div>
+        <div className="flexBetween">
+          구매 희망 금액
+          <input
+            type="number"
+            placeholder="구매 희망 금액"
+            name="purchasePrice"
+            value={purchasePrice}
+            onChange={handleChangePurchasePrice}
+          ></input>
+        </div>
+        <div className="flexBetween">
+          구매 희망 수량
+          <input
+            type="number"
+            placeholder="구매 희망 수량"
+            name="stockAmount"
+            value={stockAmount}
+            onChange={handleChangeStockAmount}
+          ></input>
+        </div>
+        <div className="flexBetween">
+          희망 목표 금액
+          <input
+            type="number"
+            placeholder="희망 목표 금액"
+            name="targetPrice"
+            value={targetPrice}
+            onChange={handleChangeTargetPrice}
+          ></input>
+        </div>
+        <div className="flexBetween">
+          절망 폭파 금액
+          <input
+            type="number"
+            placeholder="절망 폭파 금액"
+            name="stopLossPrice"
+            value={stopLossPrice}
+            onChange={handleChangeStopLossPrice}
+          ></input>
+        </div>
+        <div className="flexBetween">
+          최대 인원 수
+          <input
+            type="number"
+            placeholder="최대 인원수"
+            name="maxPerson"
+            min="1"
+            max="10"
+            value={maxPerson}
+            onChange={handleChangeMaxPerson}
+          ></input>
+        </div>
         <button type="submit">택시 생성</button>
       </form>
     </div>
