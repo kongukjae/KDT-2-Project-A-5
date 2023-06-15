ccc3bb2d5264f47be37e5fe6ca82cf5b61b34048
jangrubin <wkdfnqls2465@gmail.com>
2023-06-15 12:54:32 +0900

    받아온 데이터 테스트

--- utils/Components/showTaxi.tsx
+++ utils/Components/showTaxi.tsx
@@ -1,16 +1,21 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 const ShowTaxi = () : JSX.Element => {
+const [taxiData, setTaxiData] = useState<any>(null);
 useEffect(()=> {
+  let taxiData;
   fetch('/showTaxiData')
   .then(response => response.json())
   .then(data => {
-    console.log(data)
+    setTaxiData(data[0]['taxiId'])
+    // console.log(data)
   })
   .catch(error => {
     // 오류 처리
     console.error('정류장 데이터를 가져오는데 실패 :', error);
   });
 },[])
-  return <>테스트</>
+  return <div>
+    {taxiData}
+  </div>
 };
 export default ShowTaxi;
