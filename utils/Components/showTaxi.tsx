f155f2506617188c1bf7e05b2b13ad1f7be444fa
jangrubin <wkdfnqls2465@gmail.com>
2023-06-15 14:46:56 +0900

    DB에서 받아온 데이터 ->n번째 배열의 태그 생성(목업 참고)

--- utils/Components/showTaxi.tsx
+++ utils/Components/showTaxi.tsx
@@ -1,21 +1,49 @@
 import React, { useEffect, useState } from "react";
 const ShowTaxi = () : JSX.Element => {
-const [taxiData, setTaxiData] = useState<any>(null);
+const [taxiData, setTaxiData] = useState<any>();
+
 useEffect(()=> {
-  let taxiData;
   fetch('/showTaxiData')
   .then(response => response.json())
   .then(data => {
-    setTaxiData(data[0]['taxiId'])
-    // console.log(data)
+    setTaxiData(data)
+    console.log(data)
   })
   .catch(error => {
     // 오류 처리
     console.error('정류장 데이터를 가져오는데 실패 :', error);
   });
+  
 },[])
-  return <div>
-    {taxiData}
-  </div>
+  return (
+    <div>
+      {taxiData ? (
+        // 데이터가 존재하는 경우에만 접근
+        taxiData.map((data: any, index: number) => (
+          <div key={index}>
+            {/* 데이터 활용 */}
+            <div>{data['stocks']}</div>
+            {/* 택시 고유 번호 */}
+            <div>{data['taxiId']}</div>
+            {/* 출발 희망가 */}
+            <div>{data['purchasePrice']}</div>
+            {/* 목표가 */}
+            <div>{data['targetPrice']}</div>
+            {/* 구매량 */}
+            <div>{data['stockAmount']}</div>
+            {/* 폭파 희망가 */}
+            <div>{data['stopLossPrice']}</div>
+            {/* 동승자 모집 기간 */}
+            <div>{data['recruitmentPeriod']}</div>
+            {/* 최대 인원 */}
+            <div>{data['maxPerson']}</div>
+          </div>
+        ))
+      ) : (
+        // 데이터가 없는 경우에 대한 처리
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 };
 export default ShowTaxi;
