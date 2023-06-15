91fec48f9cb5b3e4309c475ba8b1fdf71ff2167d
gwonyejun <kyjuni1059@gmail.com>
2023-06-15 11:46:53 +0900

    키 값과 벨류 값을 나누어서 클래스에 전달 하였다.

--- src/controllers/taxiCreate.ts
+++ src/controllers/taxiCreate.ts
@@ -4,18 +4,18 @@ import dbConnect from "../../utils/DB/dbConfigure";
 
 class taxiCreatemodule {
   taxiKeyData: string[];
-  // taxiValueData: object[];
-  constructor(...taxiKeyData: string[] ) {
+  taxiValueData: unknown[];
+  constructor(taxiKeyData: string[], taxiValueData: unknown[]) {
     this.taxiKeyData = taxiKeyData;
-    // this.taxiValueData = taxiValueData;
+    this.taxiValueData = taxiValueData;
   }
 }
 
 export default function taxiCreate(req: Request, res: Response) {
   const taxiKeyData = Object.keys(req.body);
   const taxiValueData = Object.values(req.body);
-  console.log(taxiValueData,taxiKeyData)
-  const test  = new taxiCreatemodule(...taxiKeyData)
+  const test = new taxiCreatemodule(taxiKeyData, taxiValueData );
+  console.log(test);
  
   // const test = taxiCreatemodule(req.body);
   // dbConnect.query(`insert into taxi(`, (err, result) => {
