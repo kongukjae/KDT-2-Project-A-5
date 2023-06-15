03d7128f5eee3a86a3fe337b8b9f5f4bebfbe91c
wjkk21 <verstand821@gmail.com>
2023-06-15 11:41:03 +0900

    react-modal  설치     react-modal: ^3.16.1,     @types/react-modal: ^3.16.0, 입출금 창 모달로 생성 계좌 기본 금액 설정

--- utils/Components/bankAccount.tsx
+++ utils/Components/bankAccount.tsx
@@ -1,49 +1,79 @@
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import "../../src/views/css/bankAccount";
+import React, { useState, useEffect } from "react";
 import "../../src/views/css/style";
+import Modal from "react-modal";
+
+const modalStyles = {
+  content: {
+    width: "340px",
+    height: "340px",
+    margin: "0 auto",
+  },
+};
 
 export default function Account() {
-  const [balance, setBalance] = useState(0);
-  const [showModal, setShowModal] = useState(false);
-  const navigate = useNavigate();
+  const [balance, setBalance] = useState(999999);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [amount, setAmount] = useState(0);
 
-  // 계좌에 숫자가 올라갈 때마다 호출되는 함수
-  const handleDeposit = (amount: number) => {
-    const newBalance = balance + amount;
-    setBalance(newBalance);
-  };
-
-  const handleModalOpen = () => {
-    setShowModal(true);
-  };
-
-  const handleModalClose = () => {
-    setShowModal(false);
+  /*  계좌에 숫자가 올라갈 때마다 호출되는 함수 */
+  const handleDeposit = () => {
+    setBalance(balance + amount);
+    setAmount(0);
   };
 
   const handleWithdraw = () => {
-    navigate("/withdraw");
-    handleModalClose();
+    if (balance >= amount) {
+      setBalance(balance - amount);
+      setAmount(0);
+    } else {
+      alert("잔액이 부족합니다.");
+    }
   };
+
+  useEffect(() => {
+    const savedBalance = localStorage.getItem("balance");
+    if (savedBalance) {
+      setBalance(parseFloat(savedBalance));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("balance", balance.toString());
+  }, [balance]);
+
+  /* 모달  */
+  function openModal() {
+    setModalIsOpen(true);
+  }
+
+  function closeModal() {
+    setModalIsOpen(false);
+  }
 
   return (
     <div>
       <p className="myAccount">계좌 잔액: {balance.toLocaleString()}</p>
-      <button onClick={handleModalOpen}>충전</button>
-      <button onClick={handleModalOpen}>입금</button>
-      {/* 추가적인 버튼 또는 로직을 여기에 추가할 수 있습니다 */}
-      {showModal && (
-        <div className="modal">
-          <div className="modal-content">
-            <button className="close-btn" onClick={handleModalClose}>
-              X
-            </button>
-            <p>입출금 화면</p>
-            <button onClick={handleWithdraw}>출금</button>
-          </div>
-        </div>
-      )}
+      <p className="myAccount" onClick={openModal}>
+        입출금
+      </p>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Example Modal"
+        style={modalStyles}
+      >
+        <button onClick={closeModal}>X</button>
+        <div>입출금창</div>
+        <form>
+          <input
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(Number(e.target.value))}
+          />
+          <button onClick={handleDeposit}>입금</button>
+          <button onClick={handleWithdraw}>출금</button>
+        </form>
+      </Modal>
     </div>
   );
 }
