628562c8f829b6eeb87bafabbff83f741f7c8620
gwonyejun <kyjuni1059@gmail.com>
2023-06-15 11:22:51 +0900

    class를 만들어 클라이언트에서 넘어온 데이터를 한번에 값을 넣어 보았다.

--- src/controllers/taxiCreate.ts
+++ src/controllers/taxiCreate.ts
@@ -2,7 +2,20 @@ import { Request, Response } from 'express';
 import dbConnect from "../../utils/DB/dbConfigure";
 
 
-export default function taxiCreate(req: Request, res: Response){
-  console.log(req.body);
+class taxiCreatemodule {
+  taxiData: object[];
+  constructor(...taxiData: Object[]) {
+    this.taxiData = taxiData;
+  }
+}
+
+export default function taxiCreate(req: Request, res: Response) {
+  const taxiData = Object.entries(req.body);
+  const test = new taxiCreatemodule(...taxiData)
+  console.log(test)
+  // const test = taxiCreatemodule(req.body);
+  dbConnect.query('insert into taxi', (err, result) => {
+
+  });
 }
 
