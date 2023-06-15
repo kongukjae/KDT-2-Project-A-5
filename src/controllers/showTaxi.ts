465f79f0b7fd05d75fda7370391752966bbdbd30
jangrubin <wkdfnqls2465@gmail.com>
2023-06-15 12:40:28 +0900

    showTaxiData 미들웨어 위치 변경 -> 컴포넌트에서 데이터 불러오는 것 확인

--- src/controllers/showTaxi.ts
+++
@@ -1,5 +0,0 @@
-import express from "express";
-// import dbConnect from "../../utils/DB/dbConfigure";
-export default function (req: express.Request, res: express.Response) {
-  // 유저가 요청한 DB요청 처리를 할 로직
-}
--- utils/Components/showTaxi.tsx
+++ utils/Components/showTaxi.tsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 const ShowTaxi = () : JSX.Element => {
-
+useEffect(()=> {
+  fetch('/showTaxiData',{
+    method : 'GET',
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+  })
+  .catch(error => {
+    // 오류 처리
+    console.error('정류장 데이터를 가져오는데 실패 :', error);
+  });
+},[])
   return <>테스트</>
 };
 export default ShowTaxi;
