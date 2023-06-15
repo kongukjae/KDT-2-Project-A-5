5f8e88f8e52a4fdcae70efdf310a2699a82f2929
gwonyejun <kyjuni1059@gmail.com>
2023-06-15 11:40:50 +0900

    키와 벨류 나눈 버전

--- src/controllers/taxiCreate.ts
+++ src/controllers/taxiCreate.ts
@@ -3,19 +3,23 @@ import dbConnect from "../../utils/DB/dbConfigure";
 
 
 class taxiCreatemodule {
-  taxiData: object[];
-  constructor(...taxiData: Object[]) {
-    this.taxiData = taxiData;
+  taxiKeyData: string[];
+  // taxiValueData: object[];
+  constructor(...taxiKeyData: string[] ) {
+    this.taxiKeyData = taxiKeyData;
+    // this.taxiValueData = taxiValueData;
   }
 }
 
 export default function taxiCreate(req: Request, res: Response) {
-  const taxiData = Object.entries(req.body);
-  const test = new taxiCreatemodule(...taxiData)
-  console.log(test)
+  const taxiKeyData = Object.keys(req.body);
+  const taxiValueData = Object.values(req.body);
+  console.log(taxiValueData,taxiKeyData)
+  const test  = new taxiCreatemodule(...taxiKeyData)
+ 
   // const test = taxiCreatemodule(req.body);
-  dbConnect.query('insert into taxi', (err, result) => {
+  // dbConnect.query(`insert into taxi(`, (err, result) => {
 
-  });
+  // });
 }
 
