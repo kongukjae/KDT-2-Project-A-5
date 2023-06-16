d5fa9bc95e30574a9677038d070f024d758f06de
wjkk21 <verstand821@gmail.com>
2023-06-16 20:01:34 +0900

    버튼 컬러 설정

--- utils/Components/station.tsx
+++ utils/Components/station.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ShowTaxi from "./showTaxi";
-import { AiFillPlusCircle } from "react-icons/ai";
+import { AiOutlinePlusCircle } from "react-icons/ai";
 import { Link } from "react-router-dom";
 
 export default function Main() {
@@ -11,7 +11,7 @@ export default function Main() {
           <ShowTaxi />
           <div className="createTaxiButton">
             <Link to="/taxi">
-              <AiFillPlusCircle />
+              <AiOutlinePlusCircle size={40} color="#3B56D3" />
             </Link>
           </div>
         </div>
