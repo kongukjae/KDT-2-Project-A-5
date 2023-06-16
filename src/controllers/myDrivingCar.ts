cab2b6cc79355bbcf7705f878f1e75cf7c98530c
gwonyejun <kyjuni1059@gmail.com>
2023-06-16 12:21:45 +0900

    운행중인 확인 요청 서버에서 쿠키값 확인을 하였다.

--- src/controllers/myDrivingCar.ts
+++ src/controllers/myDrivingCar.ts
@@ -9,9 +9,6 @@ import dbConnect from "../../utils/DB/dbConfigure";
 // app.use(express.urlencoded({ extended: true })); // URL-encoded 형식의 본문을 파싱할 수 있도록 설정
 // // 증가률 구하기 위한 전날 날짜
 
-export default function myDrivingCarJoin(res: Response, req: Request) {
-
-  console.log('test',req.body);
-
-
+export default function myDrivingCarJoin(req: Request, res: Response) {
+  console.log('test', req.body);
 }
