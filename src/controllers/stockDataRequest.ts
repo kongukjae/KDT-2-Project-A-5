f4e2fd1892a22ed7d1ab8d7dad9e64e47166a19a
jangrubin <wkdfnqls2465@gmail.com>
2023-06-16 14:45:27 +0900

    for문 사용해서 alphavantage에 회사별로 요청 보내기, 1초에 5회 요청 제한

--- src/controllers/stockDataRequest.ts
+++ src/controllers/stockDataRequest.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 import path from "path";
 // console.log(path.join(__dirname,'../models'))
+import dotenv from "dotenv";
 import fs from "fs";
+dotenv.config({ path: "../../.env" }); // env 경로 설정
 // json 데이터 불러오기
 const readSymbolData = fs.readFileSync(path.join(__dirname,'../models/stock.data.json'), 'utf-8')
 const symbolArray = JSON.parse(readSymbolData)
@@ -10,17 +12,19 @@ console.log(symbolArray[0][0])
 // console.log("심볼리스트 :", readSymbolData)
 //! 최초 주식 데이터 요청 함수
 let stockData: any
+const apiKey = process.env.alphaApiKey;
 const stockDataRequest = async()=> {
-  try {
-    const symbol = "IBM";
-    const apiKey = process.env.alphaApiKey;
-    const response = await axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=5min&apikey=${apiKey}`)
-    stockData = response.data;
-    console.log(stockData)
-  } catch (error) {
-    console.error('주식 데이터를 받아오는데 실패했습니다', error);
+  for(let i=0; i<3; i++) {
+    try {
+      let symbol = symbolArray[i][0];
+      const response = await axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=5min&apikey=${apiKey}`)
+      stockData = response.data;
+      console.log(stockData)
+    } catch (error) {
+      console.error('주식 데이터를 받아오는데 실패했습니다', error);
+    }
+    // 서버에서 3분에 한번씩 주식데이터 요청
+    setInterval(stockDataRequest, 3 * 60 * 1000);
   }
-  // 서버에서 3분에 한번씩 주식데이터 요청
-  setInterval(stockDataRequest, 3 * 60 * 1000);
 }
-// stockDataRequest()
+stockDataRequest()
