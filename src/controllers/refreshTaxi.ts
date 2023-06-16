1904caf6d2713cba4cac1014deea55dd1f63e698
jangrubin <wkdfnqls2465@gmail.com>
2023-06-16 14:11:09 +0900

    DB 상태 변화 감지 -> 자동 랜더링(잠시 보류)

--- src/controllers/refreshTaxi.ts
+++ src/controllers/refreshTaxi.ts
@@ -1,16 +1,16 @@
-import dbConnect from "../../utils/DB/dbConfigure";
-const refreshData = () => {
-  return new Promise((resolve, reject) => {
-    dbConnect.query('SELECT COUNT(taxiID) AS count FROM taxi', (err, results: any) => {
-      if (err) {
-        console.error(err);
-        reject(err);
-      } else {
-        const count = results[0].count;
-        console.log(count);
-        resolve(count);
-      }
-    });
-  });
-};
-refreshData()
+// import dbConnect from "../../utils/DB/dbConfigure";
+// export default () => {
+//   return new Promise((resolve, reject) => {
+//     dbConnect.query('SELECT COUNT(taxiID) AS count FROM taxi', (err, results: any) => {
+//       if (err) {
+//         console.error(err);
+//         reject(err);
+//       } else {
+//         const count = results[0].count;
+//         console.log(count);
+//         resolve(count);
+//       }
+//     });
+//   });
+// };
+// refreshData()
