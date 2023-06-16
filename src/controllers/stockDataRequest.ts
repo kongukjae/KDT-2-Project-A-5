b6e636d5b8cca448ee992d6d86fc7ecfc12076f2
jangrubin <wkdfnqls2465@gmail.com>
2023-06-16 15:29:05 +0900

    주식 데이터 요청 모듈 제작하고 정상 작동확인

--- src/controllers/stockDataRequest.ts
+++ src/controllers/stockDataRequest.ts
@@ -3,8 +3,10 @@ import path from "path";
 // console.log(path.join(__dirname,'../models'))
 import dotenv from "dotenv";
 import fs from "fs";
+import { Server as SocketIOServer } from "socket.io";
 dotenv.config({ path: "../../.env" }); // env 경로 설정
-// json 데이터 불러오기
+export default (io : SocketIOServer) => {
+  // json 데이터 불러오기
 const readSymbolData = fs.readFileSync(path.join(__dirname,'../models/stock.data.json'), 'utf-8')
 const symbolArray = JSON.parse(readSymbolData)
 // 회사 이름만 떼오기
@@ -14,13 +16,14 @@ const symbolArray = JSON.parse(readSymbolData)
 let stockData: any
 const apiKey = process.env.alphaApiKey;
 const stockDataRequest = async()=> {
+  
+  // 회사 리스트만큼 요청
   for(let i=0; i<symbolArray.length; i++) {
     try {
+      // 회사명
       let symbol = symbolArray[i][0];
-      console.log(symbol)
       const response = await axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=5min&apikey=${apiKey}`)
       stockData = response.data;
-      // console.log(stockData);
     } catch (error) {
       console.error('주식 데이터를 받아오는데 실패했습니다', error);
     }
@@ -28,4 +31,22 @@ const stockDataRequest = async()=> {
   // 서버에서 3분에 한번씩 주식데이터 요청
   setInterval(stockDataRequest, 3 * 60 * 1000);
 }
-stockDataRequest()
+stockDataRequest();
+// 3분에 한번 데이터 쏴주기
+let increaseNum = 0;
+const stockDataLivetransmission = setInterval(() => {
+  try {
+    let symbol = stockData["Meta Data"]["2. Symbol"];
+    let stockObjectData: any = Object.entries(stockData['Time Series (5min)'])
+    let jsonData = JSON.stringify([symbol, stockObjectData[increaseNum]]);
+    // console.log(jsonData);
+    io.emit("stockDataUpdate", jsonData);
+    increaseNum++
+    if (increaseNum >= stockObjectData.length) {
+      clearInterval(stockDataLivetransmission);
+    }
+  } catch (error) {
+    console.error('stockDataLivetransmission 에러', error);
+  }
+}, 1 * 2000);
+}
