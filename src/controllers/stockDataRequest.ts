ef5f8085a5f2ecb3de4ef1f5ed551c66b9c902e3
jangrubin <wkdfnqls2465@gmail.com>
2023-06-16 15:11:24 +0900

    3분에 한번씩 4개 회사 데이터 요청으로 변경

--- src/controllers/stockDataRequest.ts
+++ src/controllers/stockDataRequest.ts
@@ -8,23 +8,24 @@ dotenv.config({ path: "../../.env" }); // env 경로 설정
 const readSymbolData = fs.readFileSync(path.join(__dirname,'../models/stock.data.json'), 'utf-8')
 const symbolArray = JSON.parse(readSymbolData)
 // 회사 이름만 떼오기
-console.log(symbolArray)
+// console.log(symbolArray)
 // console.log("심볼리스트 :", readSymbolData)
 //! 최초 주식 데이터 요청 함수
 let stockData: any
 const apiKey = process.env.alphaApiKey;
 const stockDataRequest = async()=> {
-  for(let i=0; i<symbolArray[i][0].length; i++) {
+  for(let i=0; i<symbolArray.length; i++) {
     try {
       let symbol = symbolArray[i][0];
+      console.log(symbol)
       const response = await axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=5min&apikey=${apiKey}`)
       stockData = response.data;
-      console.log(stockData);
+      // console.log(stockData);
     } catch (error) {
       console.error('주식 데이터를 받아오는데 실패했습니다', error);
     }
-    // 서버에서 3분에 한번씩 주식데이터 요청
-    // setInterval(stockDataRequest, 3 * 60 * 1000);
   }
+  // 서버에서 3분에 한번씩 주식데이터 요청
+  setInterval(stockDataRequest, 3 * 60 * 1000);
 }
-// stockDataRequest()
+stockDataRequest()
