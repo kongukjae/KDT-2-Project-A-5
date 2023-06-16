94c1e1dbeb59ba7b4999b41d179af99cbd09afed
박준형 <108733795+krapli441@users.noreply.github.com>
2023-06-16 16:45:38 +0900

    Area.css : 중복된 클래스명 driverArea.css로 이동. driverArea.css : Area.css에 있던 스타일 요소들 해당 파일로 이도이. driverContentsBox.tsx : 스크롤 효과를 위해 div 생성.

--- utils/Components/driverContentsBox.tsx
+++ utils/Components/driverContentsBox.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { DriverData } from "../../src/models/driver";
+import "../../src/views/css/driverArea.css";
 
 interface DriverContentsBoxProps {
   driverName: string;
@@ -21,15 +22,17 @@ const driverData = (): JSX.Element => {
       {driver.length > 0 ? (
         driver.map((element: DriverContentsBoxProps) => (
           <div className="driverContentsBox">
-            <div className="driverName">{element.driverName}</div>
-            <div className="driverOperationCount">
-              {element.driverOperationsCount}
+            <div className="driverInformation">
+              <div className="driverName">{element.driverName}</div>
+              <div className="driverOperationCount">
+                {element.driverOperationsCount}
+              </div>
+              <div className="driverNoAccidentCount">
+                {element.driverNoAccidentCount}
+              </div>
+              <div className="driverGoodTag">{element.driverGoodTag}</div>
+              <div className="driverBadTag">{element.driverBadTag}</div>
             </div>
-            <div className="driverNoAccidentCount">
-              {element.driverNoAccidentCount}
-            </div>
-            <div className="driverGoodTag">{element.driverGoodTag}</div>
-            <div className="driverBadTag">{element.driverBadTag}</div>
           </div>
         ))
       ) : (
