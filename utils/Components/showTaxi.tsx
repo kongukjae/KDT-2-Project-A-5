ab77b7f5283e926f97047b25dd0dc0b24a36e01b
jangrubin <wkdfnqls2465@gmail.com>
2023-06-16 13:09:02 +0900

    클릭한 요소만 차트 보여주게 설정

--- utils/Components/showTaxi.tsx
+++ utils/Components/showTaxi.tsx
@@ -3,9 +3,11 @@ import React, { useEffect, useState } from "react";
 // const [priceState, setPriceState] = useState<any[]>([])
 import { AiFillRobot } from "react-icons/ai";
 import "../../src/views/css/showTaxi";
+import StockContentsBox from "./stockContentsBox";
 const ShowTaxi = () : JSX.Element => {
-const [taxiData, setTaxiData] = useState<any>();
-const [defaultContainer, setDefaultContainer] = useState(false)
+const [taxiData, setTaxiData] = useState<any[]>([]);
+// const [defaultContainer, setDefaultContainer] = useState(false)
+const [showChart, setShowChart]= useState(Array(taxiData.length).fill(false))
 useEffect(()=> {
   fetch('/showTaxiData')
   .then(response => response.json())
@@ -19,8 +21,12 @@ useEffect(()=> {
   });
   
 },[])
-  const seeMore = ()=> {
-    setDefaultContainer(true);
+  const seeMore = (index : any)=> {
+    setShowChart((prevState)=> {
+      const updateState = [...prevState];
+      updateState[index] = !updateState[index];
+      return updateState;
+    })
   }
   const refreshData = ()=> {
     fetch('/showTaxiData')
@@ -31,17 +37,23 @@ useEffect(()=> {
     })
   }
   return (
-    <div id="showTaxiContainer" onClick={seeMore}>
+    <div id="showTaxiContainer">
       {taxiData ? (
         // 데이터가 존재하는 경우에만 접근
         taxiData.map((data: any, index: number) => (
-          <div key={index}>
+          <div key={index} onClick={()=> {seeMore(index)}}>
             {/* 데이터 활용 */}
             <div id="stocks">{data['stocks']}</div>
             {/* 택시 고유 번호 */}
             <div>{data['taxiId']}</div>
             {/* 출발 희망가 */}
             <div id="purchasePrice">{data['purchasePrice']}</div>
+            {/* 클릭 했을 때만 차트 랜더링 */}
+            {showChart[index] && (
+            <div>
+              <StockContentsBox />
+            </div>
+            )}
             {/* 구매량 */}
             <div className="commonFontSize">{data['stockAmount']}</div>
             {/* 목표가 */}
@@ -59,7 +71,6 @@ useEffect(()=> {
             <AiFillRobot size={24} color='gray'/>
             </div>
           </div>
-          
         ))
       ) : (
         // 데이터가 없는 경우에 대한 처리
