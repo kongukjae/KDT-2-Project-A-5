d71c16d9bfe258ffdfe81755d2b9941ba5c834d0
박준형 <108733795+krapli441@users.noreply.github.com>
2023-06-16 12:23:00 +0900

    최초 로그인 시 보여주는 애니메이션에 대한 css 속성 수정.

--- src/views/js/firstLoginPage/welcomeSecond.tsx
+++ src/views/js/firstLoginPage/welcomeSecond.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import "@dotlottie/player-component";
 import styles from "../../css/welcome.module.css";
 import { Link } from "react-router-dom";
+import exp from "constants";
+
+const explain =
+  "마음에 드는 택시를 찾아 탑승하면 \n희망 매수가에 도달했을 시 자동으로 매수합니다.";
 
 export default function SecondAnimation() {
   return (
     <div className={styles.main}>
-      <h1>스톡 투게더는...</h1>
+      <h1 className={styles.welcomeTitle}>스톡 투게더는...</h1>
       <div className={styles.welcomeContentsBox}>
         <dotlottie-player
           src="../../src/models/secondAnimation.lottie"
@@ -14,8 +18,7 @@ export default function SecondAnimation() {
           style={{ width: "347px", height: "99px" }}
         />
       </div>
-      <p>마음에 드는 택시를 찾아 탑승하면</p>
-      <p>희망 매수가에 도달했을 시 자동으로 매수합니다.</p>
+      <div className={styles.welcomeExplainText}>{explain}</div>
       <Link to="/welcome_3">
         <button>다음</button>
       </Link>
--- src/views/js/firstLoginPage/welcomeThird.tsx
+++ src/views/js/firstLoginPage/welcomeThird.tsx
@@ -3,10 +3,12 @@ import "@dotlottie/player-component";
 import styles from "../../css/welcome.module.css";
 import { Link } from "react-router-dom";
 
+const explain = "목표 금액에 도달했다면 \n자동으로 매도되어 거래가 종료됩니다.";
+
 export default function ThirdAnimation() {
   return (
     <div className={styles.main}>
-      <h1>스톡 투게더는...</h1>
+      <h1 className={styles.welcomeTitle}>스톡 투게더는...</h1>
       <div className={styles.welcomeContentsBox}>
         <dotlottie-player
           src="../../src/models/thirdAnimation.lottie"
@@ -14,8 +16,7 @@ export default function ThirdAnimation() {
           style={{ width: "347px", height: "99px" }}
         />
       </div>
-      <p>목표 금액에 도달했다면</p>
-      <p>자동으로 매도되어 거래가 종료됩니다.</p>
+      <div className={styles.welcomeExplainText}>{explain}</div>
       <Link to="/home">
         <button>확인했어요</button>
       </Link>
