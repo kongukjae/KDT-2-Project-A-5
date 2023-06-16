6920d9a782c0ddcb6f057f2bdeda3d10c9a59c0a
ParkDalJae <dv.over931@gmail.com>
2023-06-16 19:04:50 +0900

    프리티어좀 함

--- utils/Components/myTaxiList.tsx
+++ utils/Components/myTaxiList.tsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
-import "../../src/views/css/myTaxiListCss.css"
+import "../../src/views/css/myTaxiListCss.css";
 
 const MyTaxiList = (): JSX.Element => {
-  const [taxiData, setTaxiData] = useState<any[]>([]);
+  const [myTaxiData, setMyTaxiData] = useState<any[]>([]);
   useEffect(() => {
     fetch("/myTaxiData")
       .then((response) => response.json())
       .then((data) => {
-        setTaxiData(data);
+        setMyTaxiData(data);
         console.log(data);
       })
       .catch((error) => {
@@ -16,51 +16,52 @@ const MyTaxiList = (): JSX.Element => {
       });
   }, []);
 
-  const refreshData = ()=> {
-    fetch('/myTaxiData')
-    .then(response => response.json())
-    .then((data)=> {
-      setTaxiData(data)
-      console.log(data);
-    })
-  }
+  const refreshData = () => {
+    fetch("/myTaxiData")
+      .then((response) => response.json())
+      .then((data) => {
+        setMyTaxiData(data);
+      });
+  };
 
   return (
-    <div >
-      {taxiData ? (
-        // 데이터가 존재하는 경우에만 접근
-        taxiData.map((data: any, index: number) => (
-          <div key={index} className="myTaxiListCss">
-            {/* 데이터 활용 */}
-            <div id="stocks">{data["stocks"]}</div>
-            {/* 택시 고유 번호 */}
-            <div>{data["taxiId"]}</div>
-            {/* 출발 희망가 */}
-            <div id="purchasePrice">{data["purchasePrice"]}</div>
-            {/* 구매량 */}
-            <div className="commonFontSize">{data["stockAmount"]}</div>
-            {/* 목표가 */}
-            <div className="commonFontSize" style={{ color: "#008000" }}>
-              {data["targetPrice"]}
+    <>
+      <div className="main">
+        {myTaxiData ? (
+          // 데이터가 존재하는 경우에만 접근
+          myTaxiData.map((data: any, index: number) => (
+            <div key={index} className="myTaxiListCss">
+              {/* 데이터 활용 */}
+              <div id="stocks">{data["stocks"]}</div>
+              {/* 택시 고유 번호 */}
+              <div>{data["taxiId"]}</div>
+              {/* 출발 희망가 */}
+              <div id="purchasePrice">{data["purchasePrice"]}</div>
+              {/* 구매량 */}
+              <div className="commonFontSize">{data["stockAmount"]}</div>
+              {/* 목표가 */}
+              <div className="commonFontSize" style={{ color: "#008000" }}>
+                {data["targetPrice"]}
+              </div>
+              {/* 폭파 희망가 */}
+              <div className="commonFontSize" style={{ color: "#C1121F" }}>
+                {data["stopLossPrice"]}
+              </div>
+              {/* 동승자 모집 기간 */}
+              <div className="commonFontSize">{data["recruitmentPeriod"]}</div>
+              {/* 최대 인원 */}
+              <div>{data["maxPerson"]}</div>
             </div>
-            {/* 폭파 희망가 */}
-            <div className="commonFontSize" style={{ color: "#C1121F" }}>
-              {data["stopLossPrice"]}
-            </div>
-            {/* 동승자 모집 기간 */}
-            <div className="commonFontSize">{data["recruitmentPeriod"]}</div>
-            {/* 최대 인원 */}
-            <div>{data["maxPerson"]}</div>
-          </div>
-        ))
-      ) : (
-        // 데이터가 없는 경우에 대한 처리
-        <p>Loading...</p>
-      )}
-      <div>
-        <button onClick={refreshData}>새로고침</button>
+          ))
+        ) : (
+          // 데이터가 없는 경우에 대한 처리
+          <p>Loading...</p>
+        )}
+        <div>
+          <button onClick={refreshData}>새로고침</button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 export default MyTaxiList;
