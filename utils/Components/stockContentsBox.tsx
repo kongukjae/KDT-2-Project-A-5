3481f88e4cffb0d5ab47c7cf09f179d497198bbc
jangrubin <wkdfnqls2465@gmail.com>
2023-06-19 20:08:11 +0900

    콘솔 잔당 제거

--- utils/Components/stockContentsBox.tsx
+++ utils/Components/stockContentsBox.tsx
@@ -11,10 +11,8 @@ let priceArray3 : any[] = [];
 const allPrcieArray = [
   priceArray, priceArray2, priceArray3
 ];
-// console.log("allPrcieArray", allPrcieArray)
 const StockData = (): JSX.Element => {
   const socketStockData = useContext<any>(stockContext);
-  // console.log(groupedData)
   // 주식 데이터를 담을 배열
   // 데이터가 null인 동안 로딩 상태를 표시
   if (socketStockData === null) {
@@ -24,17 +22,12 @@ const StockData = (): JSX.Element => {
   if(socketStockData) {
     const groupedData = _.groupBy(socketStockData, 'symbol')
     let aaplData : any= groupedData['AAPL'];
-    console.log(aaplData, "aaplData")
     let tslaData = groupedData['TSLA'];
     let amznData = groupedData['AMZN'];
     priceArray.push(aaplData[0]['price'][1])
     priceArray2.push(tslaData[0]['price'][1])
     priceArray3.push(amznData[0]['price'][1])
-    console.log("testArray",priceArray)
   }
-  console.log("하이")
-  
-  
   const SimpleLineChart = (props:{
     _data : any[]
   }) => {
@@ -58,40 +51,13 @@ const StockData = (): JSX.Element => {
       </div>
     );
   };
-  // const SimpleLineChart2 = () => {
-  //   return (
-  //     <div>
-  //       <div className="stockChart">
-  //       <ResponsiveContainer width="100%" height={80}>
-  //         <LineChart width={110} height={40} data={priceArray2}>
-  //           <YAxis hide={true} domain={["auto", "auto"]} />
-  //           <Line
-  //             isAnimationActive={false}
-  //             type="monotone"
-  //             dataKey="1. open"
-  //             stroke="#E63946"
-  //             dot={false}
-  //           />
-  //         </LineChart>
-  //       </ResponsiveContainer>
-  //     </div>
-    
-  //     </div>
-  //   );
-  // };
   return (
     <>
-    {/* {test} */}
       {/* 배열에 데이터가 추가될 때만 차트가 렌더링 */}
       {allPrcieArray.map((element : any, index : number)=> {
-        // console.log(element, "이거다이거다")
         return <SimpleLineChart key={index} _data={element}/>
       })}
-      {/* <SimpleLineChart _data={priceArray}/> */}
-      {/* {console.log(priceArray, "element")} */}
-      {/* {console.log("하이")} */}
-      {/* <SimpleLineChart2 /> */}
-      {/* <DayRange /> */}
+
     </>
   );
 };
