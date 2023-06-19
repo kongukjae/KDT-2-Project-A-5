80d62573f99239fcddd4b6ef372e1027358b63c4
jangrubin <wkdfnqls2465@gmail.com>
2023-06-19 12:47:14 +0900

    데이터 요청하는 부분 수정

--- src/controllers/stockDataSend.ts
+++ src/controllers/stockDataSend.ts
@@ -1,28 +1,28 @@
 import { Server as SocketIOServer } from "socket.io";
 export function stockDataSend(stockData : any, io : SocketIOServer) {
-  // 주식 데이터 전송
 let increaseNum = 0;
 const stockDataLivetransmission = setInterval(async () => {
-  stockData.map((element: object | any, index : number)   => {
+  // 데이터 가공
+  const dataFormatt = stockData.map((element: object | any)   => {
+    let jsonData : any;
     try {
       // 데이터가 없다면 취소
-      if (!element) return;
       const symbol = element["Meta Data"]["2. Symbol"];
       const stockObjectData = Object.entries(element["Time Series (5min)"]);
-      const allData = [symbol, stockObjectData[increaseNum]];
-      // const jsonData = JSON.stringify([
-      //   symbol,
-      //   stockObjectData[increaseNum],
-      // ]);
-      console.log(allData)
-      io.emit("stockDataUpdate", allData);
-      increaseNum++;
-      if (increaseNum >= stockObjectData.length) {
-        clearInterval(stockDataLivetransmission);
-      }
+      jsonData = JSON.stringify([
+        symbol,
+        stockObjectData[increaseNum],
+      ]);
     } catch (error) {
       console.error("stockDataLivetransmission 에러", error);
     }
-  })
-}, 2 * 1000);
+    console.log(jsonData)
+    return jsonData;
+  });
+  // 가공 된 데이터 전송
+  const dataSend = ()=> {
+    io.emit("stockDataUpdate", dataFormatt);
+  };
+  increaseNum++
+}, 3 * 1000);
 }
