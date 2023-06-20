c444c18ab7a3764dfd9eb966c9f5033810166ddd
jangrubin <wkdfnqls2465@gmail.com>
2023-06-20 17:11:11 +0900

    또한루프

--- utils/Components/dayRange.tsx
+++ utils/Components/dayRange.tsx
@@ -1,23 +1,26 @@
 import _ from "lodash";
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import stockContext from "../../src/views/js/stockContext";
+const testArray :any = [];
 
 const DayRange = (): JSX.Element => {
-//   const [lastdata, setLastdata] = useState("");
+  const [lastdata, setLastdata] = useState("");
   const dayRangeContext = useContext<any>(stockContext);
   if (dayRangeContext === null) {
     return <div>Loading...</div>; // 데이터가 null인 동안 로딩 상태를 표시
   }
+  let Lastdata : any = []
   useEffect(()=> {
-    
+
 //   if(dayRangeContext) {
 //     //! 전날 데이터 요청에 사용하기 위해 데이터 가공
   const groupedData : any = _.groupBy(dayRangeContext, 'symbol')
   console.log(groupedData)
   Object.values(groupedData).map((data :any)=> {
     console.log(data)
-    // const dataIng = data[0].price[1]['1. open'];
-    let formattedDate;
+    const dataIng = data[0].price[1]['1. open'];
+    let formattedDate : any;
+
    try {
     let today = data[0].price[0];
     let dateOnly = today.split(" ")[0];
@@ -25,29 +28,43 @@ const DayRange = (): JSX.Element => {
     formattedDate = noHyphen.join("");
    } catch (err) {
     console.log("데이터 안왔옹")
-   }
-      fetch(`/yesterDayDataRequest`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        // fetch 요청에 23230612과 같은식으로 보내야 함
-        body: JSON.stringify({ stockName: data[0].symbol, day: formattedDate }),
-      })
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error("POST 요청이 실패했습니다.");
-        }
-      })
-      .then((data) => {
-        // setLastdata(data[0]["open"]);
-        console.log(data[0]["open"])
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+   };
+  function test() {
+      
+    fetch(`/yesterDayDataRequest`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // fetch 요청에 23230612과 같은식으로 보내야 함
+      body: JSON.stringify({ stockName: data[0].symbol, day: formattedDate }),
+    })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("POST 요청이 실패했습니다.");
+      }
+    })
+    .then((data) => {
+      // setLastdata(data[0]["open"]);
+      console.log(data[0]["open"])
+      Lastdata(data[0]["open"])
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
+    const initialValue: any = Lastdata;
+    console.log(initialValue)
+    const finalValue = parseFloat(dataIng);
+    const increasePercent = ((finalValue - initialValue) / initialValue) * 100;
+    const roundedIncreasePercent = Math.round(increasePercent * 100) / 100;
+    console.log(roundedIncreasePercent, "해결?")
+    testArray.push(roundedIncreasePercent)
+    console.log(testArray)
+    return testArray;
+  }
   })
   // const initialValue: any = lastdata;
   // const finalValue = parseFloat(dataIng);
@@ -95,7 +112,7 @@ const DayRange = (): JSX.Element => {
   
   return (
     <div className="stockInfo">
-      {/* <div className="stockSymbol">{aaplData[0].symbol}</div> */}
+      <div className="stockSymbol">{}</div>
       <div className="stockChangeRate">
         {/* {roundedIncreasePercent.toFixed(2)} % */}
       </div>
