d60f8e472e3ef6834e0cb64b6121fce3c51c66c9
jangrubin <wkdfnqls2465@gmail.com>
2023-06-20 10:19:52 +0900

    symbol 데이터  -> 객체여서 생겼던 오류

--- utils/Components/dayRange.tsx
+++ utils/Components/dayRange.tsx
@@ -1,75 +1,76 @@
-// import _ from "lodash";
-// import React, { useContext, useState } from "react";
-// import stockContext from "../../src/views/js/stockContext";
+import _ from "lodash";
+import React, { useContext, useState } from "react";
+import stockContext from "../../src/views/js/stockContext";
 
-// const DayRange = (): JSX.Element => {
-//   // const dumyData =
-//   const [lastdata, setLastdata] = useState("");
-//   const dayRangeContext = useContext<any>(stockContext);
-//   if (dayRangeContext === null) {
-//     return <div>Loading...</div>; // 데이터가 null인 동안 로딩 상태를 표시
-//   }
-//   //! 전날 데이터 요청에 사용하기 위해 데이터 가공
-//   const groupedData = _.groupBy(dayRangeContext, 'symbol')
-//   console.log(groupedData)
-//   // 각 회사 별 데이터
-//   let aaplData : any= groupedData['AAPL'];
-//   // let tslaData = groupedData['TSLA'];
-//   // let amznData = groupedData['AMZN'];
-//   console.log("amznData", aaplData[0].price[1]['1. open']);
-//   // 가격
-//   let dataIng = aaplData[0].price[1]['1. open'];
+const DayRange = (): JSX.Element => {
+  // const dumyData =
+  const [lastdata, setLastdata] = useState("");
+  const dayRangeContext = useContext<any>(stockContext);
+  if (dayRangeContext === null) {
+    return <div>Loading...</div>; // 데이터가 null인 동안 로딩 상태를 표시
+  }
+  //! 전날 데이터 요청에 사용하기 위해 데이터 가공
+  const groupedData = _.groupBy(dayRangeContext, 'symbol')
+  console.log(groupedData)
+  // 각 회사 별 데이터
+  let aaplData : any = groupedData['AAPL'];
+  let tslaData : any = groupedData['TSLA'];
+  let amznData : any = groupedData['AMZN'];
+  // 가격
+  let dataIng = aaplData[0].price[1]['1. open'];
+  console.log("데이터 확인", dataIng);
 
-//   // 초기 값
-//   // parseInt() 함수를 사용하여 문자열을 정수로 변환
-//   const initialValue: any = lastdata;
-//   // 최종 값
-//   // parseFloat() 함수를 사용하여 문자열을 부동 소수점 숫자로 변환
-//   const finalValue = parseFloat(dataIng);
 
-//   // 증가율 계산
-//   const increasePercent = ((finalValue - initialValue) / initialValue) * 100;
+  // 초기 값
+  // parseInt() 함수를 사용하여 문자열을 정수로 변환
+  const initialValue: any = lastdata;
+  // 최종 값
+  // parseFloat() 함수를 사용하여 문자열을 부동 소수점 숫자로 변환
+  const finalValue = parseFloat(dataIng);
 
-//   // 소수 둘째 자리까지 반올림
-//   const roundedIncreasePercent = Math.round(increasePercent * 100) / 100;
-//   console.log("aaplData['symbol']", aaplData[0].symbol)
-//   let today = aaplData[0].price[0];
-//   console.log("today", today)
-//   let dateOnly = today.split(" ")[0];
-//   let noHyphen = dateOnly.split("-");
-//   let formattedDate = noHyphen.join("");
-//   console.log("formattedDate", formattedDate)
-//   fetch(`/yesterDayDataRequest`, {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     // fetch 요청에 23230612과 같은식으로 보내야 함
-//     // split('-')
-//     body: JSON.stringify({ stockName: aaplData[0].symbol , day: formattedDate }),
-//   })
-//     .then((response) => {
-//       if (response.ok) {
-//         return response.json();
-//       } else {
-//         throw new Error("POST 요청이 실패했습니다.");
-//       }
-//     })
-//     .then((data) => {
-//       setLastdata(data[0]["open"]);
-//     })
-//     .catch((error) => {
-//       console.error(error);
-//     });
-//   return (
-//     <div className="stockInfo">
-//       <div className="stockSymbol">{dayRangeContext[0]}</div>
-//       <div className="stockChangeRate">
-//         {" "}
-//         {roundedIncreasePercent.toFixed(2)} %
-//       </div>
-//     </div>
-//   );
-// };
+  // 증가율 계산
+  const increasePercent = ((finalValue - initialValue) / initialValue) * 100;
 
-// export default DayRange;
+  // 소수 둘째 자리까지 반올림
+  const roundedIncreasePercent = Math.round(increasePercent * 100) / 100;
+  console.log("aaplData['symbol']", aaplData[0].symbol)
+  let today = aaplData[0].price[0];
+  console.log("today", today)
+  let dateOnly = today.split(" ")[0];
+  let noHyphen = dateOnly.split("-");
+  let formattedDate = noHyphen.join("");
+  console.log("formattedDate", formattedDate)
+  fetch(`/yesterDayDataRequest`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    // fetch 요청에 23230612과 같은식으로 보내야 함
+    // split('-')
+    body: JSON.stringify({ stockName: aaplData[0].symbol , day: formattedDate }),
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("POST 요청이 실패했습니다.");
+      }
+    })
+    .then((data) => {
+      console.log("데이터 잘 들어오나 확인",data)
+      setLastdata(data[0]["open"]);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  return (
+    <div className="stockInfo">
+      <div className="stockSymbol">{aaplData[0].symbol}</div>
+      <div className="stockChangeRate">
+        {roundedIncreasePercent.toFixed(2)} %
+      </div>
+    </div>
+  );
+};
+
+export default DayRange;
