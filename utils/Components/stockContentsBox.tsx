01e95b79f68d65ff3fd8903010298806b69954c3
jangrubin <wkdfnqls2465@gmail.com>
2023-06-20 12:13:51 +0900

    병합 및 수정

--- utils/Components/stockContentsBox.tsx
+++ utils/Components/stockContentsBox.tsx
@@ -1,7 +1,10 @@
+import "@dotlottie/player-component";
 import _ from "lodash";
 import React, { useContext } from "react";
-import { Line, LineChart, ResponsiveContainer, YAxis } from 'recharts';
-import '../../src/views/css/stockChart';
+import { Line, LineChart, ResponsiveContainer, YAxis } from "recharts";
+import styles from "../../src/views/css/loading.module.css";
+import "../../src/views/css/stockArea.css";
+import "../../src/views/css/stockChart";
 import stockContext from "../../src/views/js/stockContext";
 import DayRange from "./dayRange";
 
@@ -16,7 +19,16 @@ const StockData = (): JSX.Element => {
   // 주식 데이터를 담을 배열
   // 데이터가 null인 동안 로딩 상태를 표시
   if (socketStockData === null) {
-    return <div>Loading...</div>;
+    return (
+      <div className={styles.loading}>
+        <dotlottie-player
+          src="../../src/models/loading.lottie"
+          autoplay
+          loop
+          style={{ width: "50%", height: "100%" }}
+        />
+      </div>
+    );
   }
   // 회사별로 데이터 구분
   if(socketStockData) {
@@ -55,9 +67,10 @@ const StockData = (): JSX.Element => {
     <>
       {/* 배열에 데이터가 추가될 때만 차트가 렌더링 */}
       {allPrcieArray.map((element : any, index : number)=> {
-        return <SimpleLineChart key={index} _data={element}/>
+        return   <div className="stockContentsBox">
+        <SimpleLineChart key={index} _data={element}/>
+        <DayRange/></div>
       })}
-      <DayRange/>
     </>
   );
 };
