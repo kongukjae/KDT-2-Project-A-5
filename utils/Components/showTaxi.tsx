7c370060eb46dd1e40e8231048d41a40daf00945
박준형 <108733795+krapli441@users.noreply.github.com>
2023-06-21 16:28:46 +0900

    정류장 클릭 이벤트 수정

--- utils/Components/showTaxi.tsx
+++ utils/Components/showTaxi.tsx
@@ -5,59 +5,67 @@ import "@dotlottie/player-component";
 import styles from "../../src/views/css/loading.module.css";
 
 import StockContentsBox from "./stockContentsBox";
-const ShowTaxi = () : JSX.Element => {
-const [taxiData, setTaxiData] = useState<any[]>([]);
-// const [defaultContainer, setDefaultContainer] = useState(false)
-const [showChart, setShowChart]= useState(Array(taxiData.length).fill(false))
-useEffect(()=> {
-  fetch('/showTaxiData')
-  .then(response => response.json())
-  .then(data => {
-    setTaxiData(data)
-  })
-  .catch(error => {
-    // 오류 처리
-    console.error('정류장 데이터를 가져오는데 실패 :', error);
-  });
-  
-},[])
-  const seeMore = (index : any)=> {
-    setShowChart((prevState)=> {
+const ShowTaxi = (): JSX.Element => {
+  const [taxiData, setTaxiData] = useState<any[]>([]);
+  // const [defaultContainer, setDefaultContainer] = useState(false)
+  const [showChart, setShowChart] = useState(
+    Array(taxiData.length).fill(false)
+  );
+  useEffect(() => {
+    fetch("/showTaxiData")
+      .then((response) => response.json())
+      .then((data) => {
+        setTaxiData(data);
+      })
+      .catch((error) => {
+        // 오류 처리
+        console.error("정류장 데이터를 가져오는데 실패 :", error);
+      });
+  }, []);
+  const seeMore = (index: any) => {
+    setShowChart((prevState) => {
       const updateState = [...prevState];
       updateState[index] = !updateState[index];
       return updateState;
-    })
-  }
-  const refreshData = ()=> {
-    fetch('/showTaxiData')
-    .then(response => response.json())
-    .then((data)=> {
-      setTaxiData(data)
-      console.log(data);
-    })
-  }
+    });
+  };
+  const refreshData = () => {
+    fetch("/showTaxiData")
+      .then((response) => response.json())
+      .then((data) => {
+        setTaxiData(data);
+        console.log(data);
+      });
+  };
   return (
     <div className="showTaxiStation" onClick={seeMore}>
       {taxiData ? (
         // 데이터가 존재하는 경우에만 접근
         taxiData.map((data: any, index: number) => (
-          <div>
+          <div
+            key={index}
+            onClick={() => {
+              seeMore(index);
+            }}
+          >
             <div>
               <div className="grayColorBox" key={index}></div>
               {/* 데이터 활용 */}
-              <div className="commonFontSize">구매 수량 : {data["stockAmount"]}</div>
+              <div className="commonFontSize">
+                구매 수량 : {data["stockAmount"]}
+              </div>
               {/* 목표가 */}
             </div>
             <div id="stocks">종목명 : {data["stocks"]}</div>
             {/* 택시 고유 번호 */}
             <div>택시 번호 : {data["taxiId"]}</div>
             {/* 출발 희망가 */}
-            <div id="purchasePrice">구매희망가 : {data['purchasePrice']}</div>
+            <div id="purchasePrice">구매희망가 : {data["purchasePrice"]}</div>
             {/* 클릭 했을 때만 차트 랜더링 */}
             {showChart[index] && (
-            <div>
-              <StockContentsBox />
-            </div>
+              <div>
+                <StockContentsBox />
+              </div>
             )}
             {/* 구매량 */}
 
@@ -69,10 +77,13 @@ useEffect(()=> {
               손절가 : {data["stopLossPrice"]}
             </div>
             {/* 동승자 모집 기간 */}
-            <div className="commonFontSize">모집 기간 : {data["recruitmentPeriod"]}</div>
+            <div className="commonFontSize">
+              모집 기간 : {data["recruitmentPeriod"]}
+            </div>
             {/* 최대 인원 */}
             <div>
-              최대 인원 : {data["maxPerson"]}<br/>
+              최대 인원 : {data["maxPerson"]}
+              <br />
               <AiFillRobot size={24} color="gray" />
               <AiFillRobot size={24} color="gray" />
               <AiFillRobot size={24} color="gray" />
@@ -92,9 +103,9 @@ useEffect(()=> {
           />
         </div>
       )}
-          <div>
-            <button onClick={refreshData}>새로고침</button>
-          </div>
+      <div>
+        <button onClick={refreshData}>새로고침</button>
+      </div>
     </div>
   );
 };
