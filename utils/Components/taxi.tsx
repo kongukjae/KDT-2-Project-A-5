bd7ced1dc9a891c9a341a1e6c18bfddf261b19d5
wjkk21 <verstand821@gmail.com>
2023-06-24 15:37:11 +0900

    div gaybox 추가

--- utils/Components/taxi.tsx
+++ utils/Components/taxi.tsx
@@ -6,6 +6,7 @@ import { useNavigate, Link } from "react-router-dom";
 
 import { IoMdArrowBack } from "react-icons/io";
 import styles from "../../src/views/css/welcome.module.css";
+import style from "../../src/views/css/contentsBox.css";
 
 const customStyles = {
   content: {
@@ -126,132 +127,134 @@ const corpAutoComp = () => {
   return (
     <div className="main">
       <form action="" method="" onSubmit={handleSubmit}>
-        <div className="flexBetween">
-          <input
-            type="hidden"
-            placeholder="기사이름"
-            name="userName"
-            value={userName}
-            readOnly
-            required
-          />
-        </div>
-        <div className="flexBetween">
-          종목 이름
-          <div>
+        <div className="grayColorBox">
+          <div className="flexBetween">
             <input
-              type="text"
-              placeholder="종목을 검색해주세요"
-              name="stocks"
-              value={stocks}
-              onChange={handleChangeStocks}
-              onClick={handleOpenModal}
-              ref={inputStock}
+              type="hidden"
+              placeholder="기사이름"
+              name="userName"
+              value={userName}
               readOnly
               required
+            />
+          </div>
+          <div className="flexBetween">
+            종목 이름
+            <div>
+              <input
+                type="text"
+                placeholder="종목을 검색해주세요"
+                name="stocks"
+                value={stocks}
+                onChange={handleChangeStocks}
+                onClick={handleOpenModal}
+                ref={inputStock}
+                readOnly
+                required
+              ></input>
+              <a href="#" onClick={handleOpenModal}>
+                <button type="button" value="클릭">
+                  검색
+                </button>
+              </a>
+            </div>
+          </div>
+
+          <Modal
+            isOpen={modalIsOpen}
+            onRequestClose={handleCloseModal}
+            style={customStyles}
+          >
+            {/* 모달창 띄우기 */}
+            <div className="serachInterface">
+              <h2>원하는 종목 혹은 코드를 입력해주세요</h2>
+              <button onClick={handleCloseModal}>돌아가기</button>
+            </div>
+            <StockSearch setStocks={setStocks} closeModal={handleCloseModal} />
+            {/* 기존에 생성했던 컴포넌트*/}
+          </Modal>
+
+          <div className="flexBetween ">
+            모집 마감 기간
+            <input
+              type="date"
+              placeholder="모집 마감 기간"
+              name="recruitmentPeriod"
+              value={recruitmentPeriod}
+              onChange={handleChangeRecruitmentPeriod}
+              required
             ></input>
-            <a href="#" onClick={handleOpenModal}>
-              <button type="button" value="클릭">
-                검색
-              </button>
-            </a>
+          </div>
+          <div className="flexBetween">
+            최대 운행 기간
+            <input
+              type="date"
+              placeholder="최대 운행 기간"
+              name="maximumDrivePeriod"
+              value={maximumDrivePeriod}
+              onChange={handleChangeMaximumDrivePeriod}
+              required
+            ></input>
+          </div>
+          <div className="flexBetween">
+            구매 희망 금액
+            <input
+              type="number"
+              placeholder="구매 희망 금액"
+              name="purchasePrice"
+              value={purchasePrice}
+              onChange={handleChangePurchasePrice}
+              required
+            ></input>
+          </div>
+          <div className="flexBetween">
+            구매 희망 수량
+            <input
+              type="number"
+              placeholder="구매 희망 수량"
+              name="stockAmount"
+              value={stockAmount}
+              onChange={handleChangeStockAmount}
+              required
+            ></input>
+          </div>
+          <div className="flexBetween">
+            희망 목표 금액
+            <input
+              type="number"
+              placeholder="희망 목표 금액"
+              name="targetPrice"
+              value={targetPrice}
+              onChange={handleChangeTargetPrice}
+              required
+            ></input>
+          </div>
+          <div className="flexBetween">
+            절망 폭파 금액
+            <input
+              type="number"
+              placeholder="절망 폭파 금액"
+              name="stopLossPrice"
+              value={stopLossPrice}
+              onChange={handleChangeStopLossPrice}
+              required
+            ></input>
+          </div>
+          <div className="flexBetween">
+            최대 인원 수
+            <input
+              type="number"
+              placeholder="최대 인원수"
+              name="maxPerson"
+              min="1"
+              max="10"
+              value={maxPerson}
+              onChange={handleChangeMaxPerson}
+              required
+            ></input>
           </div>
         </div>
-
-        <Modal
-          isOpen={modalIsOpen}
-          onRequestClose={handleCloseModal}
-          style={customStyles}
-        >
-          {/* 모달창 띄우기 */}
-          <div className="serachInterface">
-            <h2>원하는 종목 혹은 코드를 입력해주세요</h2>
-            <button onClick={handleCloseModal}>돌아가기</button>
-          </div>
-          <StockSearch setStocks={setStocks} closeModal={handleCloseModal} />
-          {/* 기존에 생성했던 컴포넌트*/}
-        </Modal>
-        <div className="flexBetween">
-          모집 마감 기간
-          <input
-            type="date"
-            placeholder="모집 마감 기간"
-            name="recruitmentPeriod"
-            value={recruitmentPeriod}
-            onChange={handleChangeRecruitmentPeriod}
-            required
-          ></input>
-        </div>
-        <div className="flexBetween">
-          최대 운행 기간
-          <input
-            type="date"
-            placeholder="최대 운행 기간"
-            name="maximumDrivePeriod"
-            value={maximumDrivePeriod}
-            onChange={handleChangeMaximumDrivePeriod}
-            required
-          ></input>
-        </div>
-        <div className="flexBetween">
-          구매 희망 금액
-          <input
-            type="number"
-            placeholder="구매 희망 금액"
-            name="purchasePrice"
-            value={purchasePrice}
-            onChange={handleChangePurchasePrice}
-            required
-          ></input>
-        </div>
-        <div className="flexBetween">
-          구매 희망 수량
-          <input
-            type="number"
-            placeholder="구매 희망 수량"
-            name="stockAmount"
-            value={stockAmount}
-            onChange={handleChangeStockAmount}
-            required
-          ></input>
-        </div>
-        <div className="flexBetween">
-          희망 목표 금액
-          <input
-            type="number"
-            placeholder="희망 목표 금액"
-            name="targetPrice"
-            value={targetPrice}
-            onChange={handleChangeTargetPrice}
-            required
-          ></input>
-        </div>
-        <div className="flexBetween">
-          절망 폭파 금액
-          <input
-            type="number"
-            placeholder="절망 폭파 금액"
-            name="stopLossPrice"
-            value={stopLossPrice}
-            onChange={handleChangeStopLossPrice}
-            required
-          ></input>
-        </div>
-        <div className="flexBetween">
-          최대 인원 수
-          <input
-            type="number"
-            placeholder="최대 인원수"
-            name="maxPerson"
-            min="1"
-            max="10"
-            value={maxPerson}
-            onChange={handleChangeMaxPerson}
-            required
-          ></input>
-        </div>
-
-        <div className="flexBetween">
+        <div className="flexBetween ">
           <Link to={"/station"}>
             <div className={styles.backButton}>
               <p>돌아가기</p>
